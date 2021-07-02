925d3e0b99f37c50135bc4ee087533613beeccb6
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-07-02 18:08:44 +0200

    Add Rajdhani as mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  // purge: ['.src/**/*.{js,ts,jsx,tsx}'],
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      mono: ["Rajdhani", "monospace"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
