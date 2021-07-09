965c037ec68e86b2607a2d5169453245f37691ee
Egidio Caprino <egidio.caprino@gmail.com>
2021-07-09 10:54:29 +0200

    Add hero in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
+import React from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
+import Hero from '../components/decoration/hero';
+import heroBackground from '../../public/images/VH-hero-bg.png';
+import heroTagline from '../../public/images/VH-hero-tagline.png';
 
 const Home: React.FC = () => {
   return (
@@ -9,6 +14,16 @@ const Home: React.FC = () => {
       <div className="text-7xl">
         We <code className="text-9xl">Code</code> for the{' '}
         <span className="font-italic">animals</span>
+        <Hero imageBackground={heroBackground.src} alignment="right">
+          <p className="m-32">
+            <Image
+              src={heroTagline.src}
+              width={heroTagline.width}
+              height={heroTagline.height}
+              alt="Compassion, Creativity, Code"
+            />
+          </p>
+        </Hero>
       </div>
     </>
   );
