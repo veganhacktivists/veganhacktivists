d84231c387ca0a022343dbb167d36fb6d846e6f5
João <joaop86@hotmail.com>
2021-07-14 19:22:14 +0100

    change project section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
           <LightButton href="/about">Learn More</LightButton>
         </div>
       </Hero>
-      <div className="content-center mx-auto my-32 md:w-1/2 drop-shadow-2xl text-2xl">
+      <div className="content-center mx-auto my-32 md:w-1/2 drop-shadow-2xl text-2xl px-5">
         <Image
           src={pixelHeart.src}
           width={pixelHeart.width / 3}
@@ -73,97 +73,101 @@ const Home: React.FC = () => {
         </div>
       </div>
       <div className="bg-grey-light">
-        <div className="content-center mx-auto my-32 md:w-1/2 drop-shadow-2xl text-2xl pt-10">
-          <p className="text-grey-dark">
+        <div className="content-center mx-auto my-32 md:w-1/2 drop-shadow-2xl text-2xl pt-10 px-5">
+          <p className="text-grey-dark pb-5">
             <span className="font-italic text-3xl">Featured </span>
             <b className="text-5xl font-mono">PROJECTS</b>
           </p>
-          <div className="grid md:grid-cols-4 md:gap-2 sm:grid-cols-2 sm:gap-2 pt-16">
+          <p>
+            Every project we release is 100% free for everyone, we don’t do
+            premium versions, advertisments, or sell user data what-so-ever.
+          </p>
+          <div className="grid lg:grid-cols-4 lg:gap-4 md:grid-cols-4 md:gap-4 sm:grid-cols-2 sm:gap-4 grid-cols-1 gap-4 pt-16">
             <div>
               <a href="https://activisthub.org">
                 <Image
+                  className="rounded-2xl"
                   src={activistHub.src}
                   width={activistHub.width}
                   height={activistHub.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Activist Hub</p>
             </div>
             <div>
               <a href="https://animalrebellion.org">
                 <Image
+                  className="rounded-2xl"
                   src={animalRebellion.src}
                   width={animalRebellion.width}
                   height={animalRebellion.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Animal Rebellion</p>
             </div>
             <div>
               <Image
+                className="rounded-2xl"
                 src={animalRightsMap.src}
                 width={animalRightsMap.width}
                 height={animalRightsMap.height}
                 alt="Compassion, Creativity, Code"
               />
-              <p>Animal Rights Map</p>
             </div>
             <div>
               <a href="https://animalrightsmap.org">
                 <Image
+                  className="rounded-2xl"
                   src={dailyDozen.src}
                   width={dailyDozen.width}
                   height={dailyDozen.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Daily Dozen</p>
             </div>
             <div>
               <a href="https://dailynooch.org">
                 <Image
+                  className="rounded-2xl"
                   src={dailyNooch.src}
                   width={dailyNooch.width}
                   height={dailyNooch.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Daily Nooch</p>
             </div>
             <div>
               <a href="https://www.sehatianimalsanctuary.org">
                 <Image
+                  className="rounded-2xl"
                   src={sehati.src}
                   width={sehati.width}
                   height={sehati.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Sehati</p>
             </div>
             <div>
               <a href="https://veganbootcamp.org">
                 <Image
+                  className="rounded-2xl"
                   src={veganBootcamp.src}
                   width={veganBootcamp.width}
                   height={veganBootcamp.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Vegan Bootcamp</p>
             </div>
             <div>
               <a href="https://vegancheatsheet.org">
                 <Image
+                  className="rounded-2xl"
                   src={veganCheatSheet.src}
                   width={veganCheatSheet.width}
                   height={veganCheatSheet.height}
                   alt="Compassion, Creativity, Code"
                 />
               </a>
-              <p>Vegan Cheat Sheet</p>
             </div>
           </div>
           <div className="relative mx-auto mt-10 md:w-1/3 pb-10">
