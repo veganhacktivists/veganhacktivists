b4def9003a490561b39f492105697cc4bee37096
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-07-16 12:51:56 +0200

    Comment CMS stuff
    We need to add the ENV vars or it'll break

--- src/lib/cms/index.ts
+++ src/lib/cms/index.ts
@@ -1,42 +1,43 @@
-import type { Entry } from 'contentful';
-import { createClient } from 'contentful';
+// import type { Entry } from 'contentful';
+// import { createClient } from 'contentful';
 
-type AvaliableContentTypes = 'team' | 'teamMember';
+// type AvaliableContentTypes = 'team' | 'teamMember';
 
-const client = createClient({
-  space: process.env['CF_SPACE_ID'] || '',
-  accessToken: process.env['CF_DELIVERY_ACCESS_TOKEN'] || '',
-});
+// const client = createClient({
+//   space: process.env['CF_SPACE_ID'] || '',
+//   accessToken: process.env['CF_DELIVERY_ACCESS_TOKEN'] || '',
+// });
 
-export const previewClient = createClient({
-  space: process.env['CF_SPACE_ID'] || '',
-  accessToken: process.env['CF_PREVIEW_ACCESS_TOKEN'] || '',
-  host: 'preview.contentful.com',
-});
+// export const previewClient = createClient({
+//   space: process.env['CF_SPACE_ID'] || '',
+//   accessToken: process.env['CF_PREVIEW_ACCESS_TOKEN'] || '',
+//   host: 'preview.contentful.com',
+// });
 
-export const getById: <T>(id: string) => Promise<Entry<T>> = async (id) => {
-  return await client.getEntry(id);
-};
+// export const getById: <T>(id: string) => Promise<Entry<T>> = async (id) => {
+//   return await client.getEntry(id);
+// };
 
-export const getAllOfType: (
-  pageContentType: AvaliableContentTypes
-) => Promise<unknown[]> = async (pageContentType) => {
-  const entries = await client.getEntries({
-    content_type: pageContentType,
-  });
+// export const getAllOfType: (
+//   pageContentType: AvaliableContentTypes
+// ) => Promise<unknown[]> = async (pageContentType) => {
+//   const entries = await client.getEntries({
+//     content_type: pageContentType,
+//   });
 
-  return entries.items;
-};
+//   return entries.items;
+// };
 
-export const getAllIdsOfType: (
-  pageContentType: AvaliableContentTypes
-) => Promise<Entry<unknown>['sys']['id'][]> = async (pageContentType) => {
-  const entries = await client.getEntries({
-    content_type: pageContentType,
-    select: 'sys.id',
-  });
+// export const getAllIdsOfType: (
+//   pageContentType: AvaliableContentTypes
+// ) => Promise<Entry<unknown>['sys']['id'][]> = async (pageContentType) => {
+//   const entries = await client.getEntries({
+//     content_type: pageContentType,
+//     select: 'sys.id',
+//   });
 
-  return entries.items.map((entry) => entry.sys.id);
-};
+//   return entries.items.map((entry) => entry.sys.id);
+// };
 
-export default client;
+// export default client;
+export {};
