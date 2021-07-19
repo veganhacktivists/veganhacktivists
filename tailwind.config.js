f6b20f564d5fb5c38daec56c8e8dcafafe7bb04d
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-07-19 10:49:36 +0200

    Fix all usages of strawberry color (use pink instead)

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,6 +90,7 @@ module.exports = {
     },
     boxShadow: {
       'fill-green': `inset 550px 0 0 0 ${colors.green.DEFAULT}`,
+      'fill-pink': `inset 550px 0 0 0 ${colors.pink.DEFAULT}`,
       'fill-red': `inset 550px 0 0 0 ${colors.red.DEFAULT}`,
     },
   },
