4b99686efe3d252c3c831dae50d089a0ce5d8e60
Jesse Lee <mienaikoe@gmail.com>
2021-07-26 10:19:47 -0400

    Grant Page

---
+++ src/components/layout/grants/GrantsHero.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Hero from '../../decoration/hero';
+import heroBackground from '../../../../public/images/grants/VH-hero-grants.jpg';
+import heroTagline from '../../../../public/images/grants/VH-grants-hero-text.png';
+
+const GrantsHero: React.FC = () => {
+  return (
+    <Hero
+      imageBackground={heroBackground.src}
+      alignment="left"
+      classNameMapping={{
+        container: 'bg-center',
+      }}
+    >
+      <div>
+        <Image
+          src={heroTagline.src}
+          width={heroTagline.width * 0.5}
+          height={heroTagline.height * 0.5}
+          alt="Grants for a kinder world"
+          layout="intrinsic"
+          priority
+        />
+      </div>
+    </Hero>
+  );
+};
+
+export default GrantsHero;
