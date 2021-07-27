eaec5b014c9fc73c10717e98567f8b8a64f1bba6
Egidio Caprino <egidio.caprino@gmail.com>
2021-07-27 11:38:44 +0200

    Center lamp image in suggest-an-idea component

--- src/components/layout/suggestAnIdea.tsx
+++ src/components/layout/suggestAnIdea.tsx
@@ -5,13 +5,15 @@ import lampImage from '../../../public/images/Services-icon-project.png';
 
 const SuggestAnIdea: React.FC = () => (
   <div className="bg-gray md:p-36 flex flex-row justify-center">
-    <Image
-      alt="Lamp"
-      src={lampImage.src}
-      width={300}
-      height={300}
-      objectFit="cover"
-    />
+    <div className="bg-green flex p-2 md:p-8">
+      <Image
+        alt="Lamp"
+        src={lampImage.src}
+        width={300}
+        height={300}
+        objectFit="contain"
+      />
+    </div>
     <div className="bg-gray-background text-left p-3 md:p-10">
       <h4 className="text-2xl mb-3 md:mb-8">Have an idea for a project?</h4>
       <p className="mb-3 md:mb-8">
