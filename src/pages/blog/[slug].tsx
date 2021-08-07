a8f25e003d9a22e0a924e0deb6f9fe06469a9e1f
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-08-07 16:09:53 +0200

    Add basic blog and blog preview support
    TODO: Add secret token to access preview

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,31 +1,67 @@
 import type { GetStaticPaths, GetStaticProps } from 'next';
+import {
+  getAllBlogSlugs,
+  getBlogPreviewBySlug,
+  getContents,
+} from '../../lib/cms';
+import type { IBlogEntry } from '../../types/generated/contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-interface BlogEntry {
-  slug: string;
-  text: string;
+interface BlogEntryProps {
+  blog: IBlogEntry;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return { paths: [{ params: { slug: '1' } }], fallback: true };
+  const allSlugs = await getAllBlogSlugs();
+
+  return {
+    paths: allSlugs.map((slug) => ({ params: { slug } })),
+    fallback: true,
+  };
 };
 
-export const getStaticProps: GetStaticProps<
-  BlogEntry,
-  Pick<BlogEntry, 'slug'>
-> = async ({ params }) => {
-  if (!params) {
+export const getStaticProps: GetStaticProps = async ({
+  params = {},
+  preview = false,
+}) => {
+  const blog = await getEntryOrPreview(params.slug as string, preview);
+  if (!blog) {
     return {
       notFound: true,
     };
   }
 
-  const blog = { slug: params.slug, text: '' };
-
-  return { props: blog };
+  return {
+    props: {
+      blog,
+    },
+  };
 };
 
-const BlogEntry: React.FC<BlogEntry> = ({ slug }) => (
-  <div>Blog with ID {slug}</div>
-);
+const getEntryOrPreview: (
+  slug: string,
+  preview: boolean
+) => Promise<IBlogEntry> = async (slug, preview) => {
+  if (preview) {
+    return await getBlogPreviewBySlug(slug);
+  }
+
+  return (await getContents('blogEntry', { slug }))[0] as IBlogEntry;
+};
+
+const BlogEntry: React.FC<BlogEntryProps> = ({ blog }) => {
+  if (!blog) {
+    return <div>Loading...</div>;
+  }
+
+  const { slug, author, content } = blog.fields;
+  return (
+    <>
+      <div>Blog with slug {slug}</div>
+      {documentToReactComponents(content)}
+      <div>Author: {author.fields.name}</div>
+    </>
+  );
+};
 
 export default BlogEntry;
