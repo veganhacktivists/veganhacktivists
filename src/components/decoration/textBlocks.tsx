e10063778ec0df01e6d7f1cafadd9f791df7ad6f
davidvanbeveren <david.vanbeveren@gmail.com>
2021-08-07 15:45:29 +0800

    Update textBlocks.tsx

--- src/components/decoration/textBlocks.tsx
+++ src/components/decoration/textBlocks.tsx
@@ -12,7 +12,7 @@ export const FirstSubSection: React.FC<SubSectionContent> = ({
 
   return (
     <div className="text-grey content-center mx-auto my-20 md:w-1/2 drop-shadow-2xl text-2xl">
-      <h1 className="mb-16">
+      <h1 className="mb-8">
         <span className="font-italic text-3xl">{firstWord}</span>{' '}
         <span className="font-mono font-bold text-5xl uppercase text-black">
           {remainingWords}
