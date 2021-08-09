101830ebb176a3f444601c33b223a33e68df529c
Egidio Caprino <egidio.caprino@gmail.com>
2021-08-09 19:25:30 +0200

    Add Join Us page

---
+++ src/components/layout/candidateRequirement.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface CandidateRequirementProps {
+  image: StaticImageData;
+  description: string;
+  color: 'green' | 'yellow' | 'magenta' | 'orange';
+}
+
+const CandidateRequirement: React.FC<CandidateRequirementProps> = ({
+  image,
+  description,
+  color,
+}) => (
+  <div className="flex flex-row items-center gap-4">
+    <div className={`flex-shrink w-1/6 bg-${color}`}>
+      <Image alt={description} src={image} width={100} height={100} />
+    </div>
+    <p className="flex-grow w-5/6 text-left md:text-3xl">{description}</p>
+  </div>
+);
+
+export default CandidateRequirement;
