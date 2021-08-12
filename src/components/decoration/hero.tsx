f5c7d0cca474860bdd63171e8a1fe1511ecb4c56
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-08-12 11:48:04 +0200

    Force hero tagline height
    This way we won't need same height images,it will automatically adjust

--- src/components/decoration/hero.tsx
+++ src/components/decoration/hero.tsx
@@ -47,7 +47,7 @@ const Hero: React.FC<HeroProps> = ({
     classNameMapping?.content
   );
 
-  const taglineRatio = 0.6;
+  const taglineHeight = 566 * 0.6;
 
   return (
     <div className={containerClasses}>
@@ -68,11 +68,12 @@ const Hero: React.FC<HeroProps> = ({
             })}
           >
             <Image
-              layout="intrinsic"
               src={tagline.image}
               alt={tagline.alt}
-              width={tagline.image.width * taglineRatio}
-              height={tagline.image.height * taglineRatio}
+              width={
+                (tagline.image.width / tagline.image.height) * taglineHeight
+              }
+              height={taglineHeight}
               priority
             />
           </div>
