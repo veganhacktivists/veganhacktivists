fc521cf108de318b391c00712e471a1c415b2698
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-08-15 18:06:23 +0200

    Slow down cow
    Correctly move to the very edge of pixels

--- src/components/decoration/sprite.tsx
+++ src/components/decoration/sprite.tsx
@@ -6,6 +6,7 @@ import chicken from '../../../public/images/sprite_chicken.gif';
 import pig from '../../../public/images/sprite_pig.gif';
 import sheep from '../../../public/images/sprite_sheep.gif';
 import cow from '../../../public/images/sprite_cow.gif';
+import useWindowSize from '../../hooks/useWindowSize';
 
 interface SpriteProps {
   image: StaticImageData;
@@ -15,29 +16,41 @@ interface SpriteProps {
 
 const Sprite: React.FC<SpriteProps> = ({
   image,
-  secondsToTraverse = 10,
+  secondsToTraverse = 60,
   scale = 0.5,
 }) => {
   const [reverse, setReverse] = useState<boolean>(false);
 
-  const initialPosition = '15vw';
+  const { width } = useWindowSize();
+
+  const initialPositionPx = 3 * 64;
+  const finalPositionPx = (width || 0) - 16;
+  const initialPosition = `${initialPositionPx}px`;
+  const finalPosition = `${finalPositionPx}px`;
+
   const spring = useSpring({
     from: {
       left: initialPosition,
     },
     to: {
-      left: '85vw',
+      left: finalPosition,
     },
     reverse: reverse,
     onRest: () => {
       setReverse((reverse) => !reverse);
     },
-    config: { duration: secondsToTraverse * 1000 },
+    config: {
+      duration: secondsToTraverse * 1000,
+    },
   });
+
+  if (finalPositionPx <= 0 || initialPositionPx >= finalPositionPx) {
+    return null;
+  }
 
   return (
     <animated.div
-      className="none md:absolute"
+      className="absolute overflow-hidden"
       style={{
         ...spring,
         height: image.height * scale,
@@ -51,6 +64,7 @@ const Sprite: React.FC<SpriteProps> = ({
         width={image.width * scale}
         alt=""
         loading="eager"
+        layout="fixed"
       />
     </animated.div>
   );
