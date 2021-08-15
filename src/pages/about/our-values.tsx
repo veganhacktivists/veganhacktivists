59b2c00f5ca8edb17e5235bdc2ffa72509e1d357
davidvanbeveren <david.vanbeveren@gmail.com>
2021-08-15 11:15:37 +0800

    Forgot one!

--- src/pages/about/our-values.tsx
+++ src/pages/about/our-values.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import VH_PixelChicken from '../../public/images/VH_PixelChicken.png';
+import PixelChicken from '../../public/images/VH_PixelChicken.png';
 import { AboutButtons, AboutHero } from '../../components/layout/about';
 import {
   FirstSubSection,
