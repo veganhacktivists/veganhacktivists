5f63c6571d87dd8ea756b88684ea04403d55e365
davidvanbeveren <david.vanbeveren@gmail.com>
2021-08-15 17:32:14 +0800

    Our values text update

--- src/pages/about/our-values.tsx
+++ src/pages/about/our-values.tsx
@@ -17,11 +17,11 @@ const OurMission: React.FC = () => {
       <div className="m-10">
         <AboutButtons />
         <FirstSubSection header="Our values">
-          We know that embracing strong core values for our organization is
-          vital in recruiting diverse and talented volunteers from all over the
-          world. By making our community welcoming and safe for everyone, we
-          build a more productive environment in which all of us can thrive and
-          be the best version of ourselves when fighting for the animals.
+          We embrace strong core values for our organization and recruit 
+		  diverse and talented volunteers. By making our community open 
+		  and safe for everyone, we build an environment in which all of 
+		  us can be the best version of ourselves when fighting for the 
+		  animals.
         </FirstSubSection>
         <Image
           src={PixelChicken.src}
