5e5c2e52ea974fb19a2ed9f011862aed2bc0e0b7
Stephan de Vries <stephan281094@gmail.com>
2021-08-16 15:04:00 +0200

    Add info box component

---
+++ src/__tests__/components/info-box.tsx
@@ -0,0 +1,55 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import React from 'react';
+import InfoBox from '../../components/info-box';
+import lampImage from '../../public/images/Services-icon-project.png';
+
+it('should display the title correctly', () => {
+  const { getByText } = render(
+    <InfoBox
+      title="Have an idea for a project?"
+      icon={lampImage}
+      iconBgColor="green"
+      iconAccentColor="green-dark"
+    >
+      Lorem impsum dolor etc.
+    </InfoBox>
+  );
+
+  const title = getByText('Have an idea for a project?');
+  expect(title).toBeInTheDocument();
+});
+
+it('should display the content correctly', () => {
+  const { getByText } = render(
+    <InfoBox
+      title="Have an idea for a project?"
+      icon={lampImage}
+      iconBgColor="green"
+      iconAccentColor="green-dark"
+    >
+      <h1>Content</h1>
+      <p>In the form of HTML, instead of a string</p>
+    </InfoBox>
+  );
+
+  const content = getByText('Content');
+  expect(content).toBeInTheDocument();
+});
+
+it('should align the info box correctly', () => {
+  const { container } = render(
+    <InfoBox
+      title="Have an idea for a project?"
+      icon={lampImage}
+      iconBgColor="green"
+      iconAccentColor="green-dark"
+      align="right"
+    >
+      Blablabla
+    </InfoBox>
+  );
+
+  const element = container.querySelector('.flex-row-reverse');
+  expect(element).toBeInTheDocument();
+});
