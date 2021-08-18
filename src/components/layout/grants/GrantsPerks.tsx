8ae54889906d4908a3a869ba71c102b267e017ec
davidvanbeveren <david.vanbeveren@gmail.com>
2021-08-18 14:21:08 +0800

    Grant page updates
    Add qualification notice and button
    Remove apply here link, text changes
    More padding
    Add import for LightButton
    Remove free
    Grant text updates
    Remove center tags and use tailwind classes
    Use tailwind class for width
    Fix &apos;
    center button

--- src/components/layout/grants/GrantsPerks.tsx
+++ src/components/layout/grants/GrantsPerks.tsx
@@ -14,7 +14,7 @@ const GrantsPerks: React.FC = () => {
         In addition to $1000 in seed funding, VH Grants include:
       </h3>
       <h4 className="text-3xl font-semibold mt-12">
-        Free SquareSpace website subscription
+        SquareSpace website subscription
       </h4>
       <p className="text-2xl mt-4 leading-loose">
         <div>Valued at around $200 a year, or $17 a month.</div>
@@ -42,7 +42,7 @@ const GrantsPerks: React.FC = () => {
       </p>
 
       <h4 className="text-3xl font-semibold mt-12">
-        Free Monthly advisory calls
+        Monthly advisory calls
       </h4>
       <p className="text-2xl mt-4 leading-loose">
         <div>
