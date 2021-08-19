389fc5dbb951080c5cf57f540ab813e3b5132c67
Stephan de Vries <stephan281094@gmail.com>
2021-08-19 10:24:42 +0200

    Add missing meta tags for favicon and manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,28 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@1,500&family=PT+Sans:wght@400;700&family=Rajdhani:wght@500;600;700&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#161919" />
+          <meta name="msapplication-TileColor" content="#161919" />
+          <meta name="theme-color" content="#161919" />
         </Head>
         <body>
           <Main />
