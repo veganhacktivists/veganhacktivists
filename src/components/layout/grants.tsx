255c81697690b8466c11609298b396f88561af81
davidvanbeveren <david.vanbeveren@gmail.com>
2021-08-21 15:32:13 +0800

    Update grants.tsx

--- src/components/layout/grants.tsx
+++ src/components/layout/grants.tsx
@@ -6,7 +6,7 @@ import { DarkButton } from '../decoration/buttons';
 const Grants: React.FC = () => {
   return (
     <div className="bg-gray-background pt-14 pb-18 space-y-0 pb-12">
-	<div className="content-center mx-auto md:w-1/2 drop-shadow-2xl text-2xl pt-16">
+	<div className="content-center mx-auto md:w-1/2 drop-shadow-2xl text-2xl pt-12 pb-12">
       <div className="w-2/4 inline-block">
         <div className="bg-white">
           <div className="overflow-hidden w-full h-40">
@@ -41,7 +41,7 @@ const Grants: React.FC = () => {
             />
           </div>
           <div className="text-left ml-12 break-words max-w-sm pb-8">
-            <p className="mb-12 text-grey-dark pt-12 pb-12">
+            <p className="mb-12 text-grey-dark pt-16">
               <b className="text-4xl font-mono font-semibold">OUR 2020 ANNUAL IMPACT REVIEW</b>
             </p>
             <p className="mb-12 text-2xl">
