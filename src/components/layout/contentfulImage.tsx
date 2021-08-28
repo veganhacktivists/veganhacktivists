41bb16110b65c299177e6071e28b388d60629d13
speleothem <speleothem@users.noreply.github.com>
2021-08-28 09:39:06 -0400

    partners page setup, incomplete styles

--- src/components/layout/contentfulImage.tsx
+++ src/components/layout/contentfulImage.tsx
@@ -22,6 +22,9 @@ const ContentfulImage: React.FC<ContentfulImageProps> = ({
   }
 
   const { width, height } = details.image;
+
+  console.log({ width, height });
+
   return (
     <Image
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
--- src/pages/people/partners.tsx
+++ src/pages/people/partners.tsx
@@ -6,6 +6,12 @@ import SquareField from '../../components/decoration/squares';
 import PixelHeart from '../../../public/images/VH_PixelHeart.png';
 import JoinTheTeam from '../../components/layout/joinTheTeam';
 import Image from 'next/image';
+import { GetStaticProps } from 'next';
+import React from 'react';
+import { getContents } from '../../lib/cms';
+import { ITeamFields, ITeamMember } from '../../types/generated/contentful';
+import ContentfulImage from '../../components/layout/contentfulImage';
+import { DarkButton } from '../../components/decoration/buttons';
 
 const TEAM_SQUARES1 = [
   { color: 'grey-light', size: 16, left: 0, bottom: 0 },
@@ -21,17 +27,91 @@ const TEAM_SQUARES2 = [
   { color: 'grey', size: 16, right: 16, top: 0 },
 ];
 
-const People: React.FC = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const partners = await getContents<ITeamFields>({
+    contentType: 'teamMember',
+    query: {
+      type: 'partner',
+    },
+  });
+  return {
+    props: { partners },
+    revalidate: 120,
+  };
+};
+
+const PartnerCard: React.FC<{ partner: ITeamMember }> = ({ partner }) => {
+  const {
+    name,
+    image,
+    description: descriptionContent,
+    socialLinks,
+  } = partner.fields;
+
+  const website = socialLinks?.fields.website;
+  const domainRegEx = /https?:\/\/(?:www\.)?(?<domain>[a-zA-Z0-9]+\.[a-z]+)\//;
+  const domain = website?.match(domainRegEx)?.groups?.domain;
+
+  // @ts-expect-error ts doesnt think value should be there
+  const description = descriptionContent?.content[0].content[0].value;
+
+  console.log(image?.fields.file.details);
+
+  const imageSize = 600;
+  return (
+    <div className="flex flex-col md:flex-row justify-between mb-10">
+      <div className="bg-grey flex-shrink">
+        {image && (
+          <ContentfulImage
+            image={image}
+            height={imageSize}
+            width={imageSize}
+            alt={name}
+          />
+        )}
+      </div>
+      <div className="bg-grey-light p-10">
+        <div>
+          <span className="mx-1 text-2xl font-bold">{name}</span>
+          <span className="font-bold text-m uppercase text-grey">
+            Partner Since 2020
+          </span>
+        </div>
+        <div className="text-left text-lg mb-8">{description}</div>
+        <DarkButton className="w-64" href={website}>
+          {domain ?? name}
+        </DarkButton>
+      </div>
+    </div>
+  );
+};
+
+const PartnerList: React.FC<{ partners: ITeamMember[] }> = ({ partners }) => {
+  const ps = partners.slice(1, 10);
+  return (
+    <div className="md:mx-auto w-3/4">
+      {ps.map((p) => (
+        <PartnerCard key={p.sys.id} partner={p} />
+      ))}
+    </div>
+  );
+};
+
+const Partners: React.FC<{ partners: ITeamMember[] }> = ({ partners }) => {
   return (
     <>
       <Head>
         <title>Our Partners | Vegan Hacktivists</title>
       </Head>
       <PeopleHero />
-      <div className="m-10">
+      <div className="m-10 mb-36">
         <PeopleButtons />
-        <FirstSubSection header="Our partners">{null}</FirstSubSection>
-        <div className="m-10 mb-40">Some People exist</div>
+        <FirstSubSection header="Our partners">
+          Here are our fantastic partners whom we support and are spported by.
+          Take a look at them below, visit them, and support the amazing work
+          they do!
+        </FirstSubSection>
+        <PartnerList partners={partners} />
       </div>
       <SquareField squares={TEAM_SQUARES1} className="hidden md:block" />
       <div className="bg-grey-light pb-10 pt-16 px-10">
@@ -56,4 +136,4 @@ const People: React.FC = () => {
   );
 };
 
-export default People;
+export default Partners;
