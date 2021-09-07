23421ef111288c1b4c01f00021ce96d0d91a52dd
Ben Bowers <benjambowers@gmail.com>
2021-09-07 02:08:48 -0400

    Added a 16px option for borderWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -87,6 +87,16 @@ module.exports = {
       sans: 'PT Sans',
       italic: 'Bitter',
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      16: '16px',
+    },
   },
   variants: {
     extend: {},
