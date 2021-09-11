97105428e0ae5167d70e36f53eb531447a720f9c
Ben Bowers <benjambowers@gmail.com>
2021-09-10 20:24:11 -0400

    Fixed tailwind border color issue

--- src/components/layout/review2020/HighlightBlock.tsx
+++ src/components/layout/review2020/HighlightBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useThemeColor from '../../../hooks/useThemeColor';
 
 interface HighlightBlockProps {
   borderColor: string;
@@ -17,7 +18,8 @@ export const HighlightBlock: React.FC<HighlightBlockProps> = ({
   return (
     <div className="flex flex-col md:flex-row w-4/5 xl:w-3/5 2xl:w-1/2 mx-auto mb-8">
       <h1
-        className={`leading-normal text-left flex-1 font-mono bg-black text-white text-5xl p-10 border-l-16 border-${borderColor}`}
+        className="leading-normal text-left flex-1 font-mono bg-black text-white text-5xl p-10 border-l-16"
+        style={{ borderColor: useThemeColor(borderColor) }}
       >
         {headerStart} <span className="font-bold">{headerBold}</span>{' '}
         {headerEnd}
