1cdc06d4e0f641ca5a9f6c85f4715af927b48123
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-09-13 17:18:03 +0200

    Change report summary line height

--- src/components/layout/review2020/HighlightBlock.tsx
+++ src/components/layout/review2020/HighlightBlock.tsx
@@ -18,11 +18,10 @@ export const HighlightBlock: React.FC<HighlightBlockProps> = ({
   return (
     <div className="flex flex-col md:flex-row w-4/5 xl:w-2/3 2xl:w-2/3 mx-auto mb-8">
       <h1
-        className="leading-normal text-left flex-1 font-mono bg-black text-white text-4xl md:text-5xl p-10 border-l-16"
+        className="md:leading-[1.3] text-left flex-1 font-mono bg-black text-white text-4xl md:text-5xl p-10 border-l-16"
         style={{ borderColor: useThemeColor(borderColor) }}
       >
-        {headerStart} <span className="font-bold">{headerBold}</span>{' '}
-        {headerEnd}
+        {headerStart} <b>{headerBold}</b> {headerEnd}
       </h1>
       <p className="text-center md:text-left flex-1 text-2xl p-10 bg-grey-background">
         {children}
