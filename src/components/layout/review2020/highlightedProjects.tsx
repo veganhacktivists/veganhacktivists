3f97a422bb2d6497c0cd8fe5fe3e92613c4c4d4a
davidvanbeveren <david.vanbeveren@gmail.com>
2021-09-14 17:24:08 +0200

    Better highlighted project text

--- src/components/layout/review2020/highlightedProjects.tsx
+++ src/components/layout/review2020/highlightedProjects.tsx
@@ -21,49 +21,41 @@ const PROJECTS = {
     url: 'animalrightsmap.org',
     content:
       // eslint-disable-next-line quotes
-      `Knowing how hard it is to find local activist groups, we wanted to create a map
-			that would include every group out there, both small local groups and larger organizations
-			with thousands of groups. If you're a vegan who wants to get active, we want to make it
-			as easy as possible for you to be able to find and join groups in your local area. And
-			that's just what we did! With over 2,500 groups, browse the largest collection of animal
-			rights activist groups all located in one single map. Filter and learn more information
-			on which vegan activist organizations you‘d like to join in your area. We worked with a
-			few organizations to import new groups automatically, and we have a dedicated volunteer
-			(who built the database for this project) who meticulously updates the map every week.
-			We're really happy with how this project turned out, and we've received a lot of great
-			feedback from people who were looking to get active who found this project super useful!
-			We plan on gathering more data soon and expanding the features of this map for 2021.`,
+      `With over 2,500 groups, the Animal Rights Map is a globally updated map that helps 
+      vegans find local groups to get active with. Our map includes everyone from the 
+      largest organizations to the very small grassroots groups spread around the country. 
+      We worked with a few organizations to import new groups automatically, and we have a 
+      dedicated volunteer (that started this project) who meticulously updates the map 
+      almost every day. We've received a lot of great feedback for the map from vegans 
+      who were looking to get active - we plan on gathering more data soon and expanding 
+      the features of this map for 2021!`,
   },
   dailynooch: {
     title: 'Daily Nooch',
     url: 'dailynooch.org',
     content:
       // eslint-disable-next-line quotes
-      `With this project we wanted to create something a little more fun and light-weight,
-			something Vegans could enjoy opening, consuming and sharing with the world. Daily
-			Nooch is your one-stop source for daily vegan news, resources and inspiration! Designed
-			to be your homepage, get the latest news, quotes, art, memes, facts, videos, and much
-			much more updated every day. You're guaranteed to always find something new or inspirational
-			every single day. This project is very experimental and we don't know if vegans will use
-			this consistently, but if they do we plan on adding more customization options and more widgets.
-			In the meantime the team had a lot of fun building this project, we're really happy with the
-			cute design, and we've been using it since before the release ourselves to get our daily fix!`,
+      `With this project we wanted to create something a little more fun and light-weight that 
+      vegans could enjoy consuming and sharing with the world. Daily Nooch is your one-stop 
+      source for daily vegan news, resources and inspiration. Designed to be your homepage, 
+      get the latest news, quotes, art, memes, facts, videos, and more updated every day at 
+      midnight. This project is very experimental and we don't know if vegans will use this 
+      consistently, but in the meantime the team had a lot of fun building it. If folks like 
+      it we have a bunch of fun ideas to explore that will add more interactivity to the project.`,
   },
   mydailydozen: {
     title: 'My Daily Dozen',
     url: 'mydailydozen.org',
     content:
       // eslint-disable-next-line quotes
-      `Many of us are fans of Dr. Greger's work as he's a well known doctor in the vegan movement.
-			Dr. Greger, founder of NutritionFacts.org, created an app called "Daily Dozen" that allows
-			you to track your diet and make sure you get the best nutrition possible - and details the
-			healthiest foods and how many servings of each we should try to check off every day. We
-			wanted to expand on this concept and create a web-based version of his app with some additional
-			features. Use My Daily Dozen to keep daily track of the foods recommended by Dr. Greger in his
-			New York Times Bestselling book, How Not to Die, and now his new book, How Not to Diet! Dr.
-			Greger’s Daily Dozen. Normally we don't work on plant-based only projects but we do recognize
-			that there are many paths to Veganism and Dr. Gregers Daily Dozen project allows potential
-			non-vegans to easily adopt a healthy lifestyle.`,
+      `Dr. Greger, founder of NutritionFacts.org, created an app called "Daily Dozen" that allows 
+      you to track your diet and make sure you get the best nutrition possible - and details the 
+      healthiest foods and how many servings of each we should try to check off every day. 
+      We wanted to expand on this concept and create a web-based version of his app with 
+      some additional features. Use My Daily Dozen to keep daily track of the foods 
+      recommended by Dr. Greger in his New York Times Bestselling book, How Not to Die. 
+      We hope that this project will give non-vegans the opportunity for an easier path 
+      to veganism by adopting a plant-based lifestyle.`,
   },
 };
 
