2f532ec69e131480c75ac59a316f93c493966675
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-09-18 16:31:30 +0200

    Create /contact page to avoid past links errors
    Will need a cooler design later, this is temporal

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import ContactUsForm from '../components/forms/contactUs';
+
+const Contact: React.FC = () => {
+  return (
+    <div className="bg-grey-background">
+      <ContactUsForm />
+    </div>
+  );
+};
+
+export default Contact;
