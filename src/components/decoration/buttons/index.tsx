1b6c67d4d65e3a9ada58aa1cbff6a760ea6f1795
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-10-27 10:51:51 +0200

    Fix active DarkButton having green bg

--- src/components/decoration/buttons/index.tsx
+++ src/components/decoration/buttons/index.tsx
@@ -110,7 +110,10 @@ const DarkButton: React.FC<ButtonProps> = ({
   ...props
 }) => {
   return (
-    <FillBackground base={active ? 'magenta' : 'grey-dark'} fill="green">
+    <FillBackground
+      base={active ? 'magenta' : 'grey-dark'}
+      fill={active ? 'magenta' : 'green'}
+    >
       <BaseButton
         {...props}
         className={classNames(
