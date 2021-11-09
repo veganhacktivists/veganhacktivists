dbe1bbf71c8e2a28f2f16eb43a45a6eb853f37dd
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-11-09 17:10:12 +0100

    Fix some types

--- src/hooks/useWindowBreakpoint.ts
+++ src/hooks/useWindowBreakpoint.ts
@@ -1,14 +1,15 @@
 import tailwindConfig from '../../tailwind.config';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import { useMemo } from 'react';
+import type { TailwindConfig } from 'tailwindcss/tailwind-config';
 
-const fullConfig = resolveConfig(tailwindConfig as any);
+const fullConfig = resolveConfig(tailwindConfig as unknown as TailwindConfig);
 
 const useWindowBreakpoint: (size: 'sm' | 'md' | 'lg' | 'xl' | '2xl') => number =
   (size) => {
     const breakpointStr = useMemo(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      () => fullConfig.theme.screens![size],
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-non-null-assertion
+      () => (fullConfig.theme.screens! as any)[size],
       [size]
     );
 
