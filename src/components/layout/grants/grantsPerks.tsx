0b95a11f2457b8d868acc79e3b6aaea30197212b
davidvanbeveren <david.vanbeveren@gmail.com>
2021-12-01 20:10:14 +0100

    I hate everything

--- src/components/layout/grants/grantsPerks.tsx
+++ src/components/layout/grants/grantsPerks.tsx
@@ -16,8 +16,8 @@ const PERKS: PerkProps[] = [
     title: 'SquareSpace website subscription',
     children: (
       <>
-        <div>Valued at around $200, we'll pay for the first year!</div>
-        <div>You own it - we'll help you design and maintain it!</div>
+        <div>Valued at around $200, we&apos;ll pay for the first year!</div>
+        <div>You own it - we&apos;ll help you design and maintain it!</div>
         <div>Easy to update with little to no experience.</div>
       </>
     ),
