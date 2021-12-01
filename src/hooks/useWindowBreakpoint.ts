83893ae262763df8044bf860574dd89a4bf9be21
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-12-01 21:39:01 +0100

    Upgrade minor deps

--- src/hooks/useWindowBreakpoint.ts
+++ src/hooks/useWindowBreakpoint.ts
@@ -1,18 +1,18 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import tailwindConfig from '../../tailwind.config';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import { useMemo } from 'react';
 
 const fullConfig = resolveConfig(tailwindConfig as any);
 
-const useWindowBreakpoint: (size: 'sm' | 'md' | 'lg' | 'xl' | '2xl') => number =
-  (size) => {
-    const breakpointStr = useMemo(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      () => fullConfig.theme.screens![size],
-      [size]
-    );
+const useWindowBreakpoint: (
+  size: 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+) => number = (size) => {
+  // @ts-expect-error
+  const breakpointStr = useMemo(() => fullConfig.theme.screens![size], [size]);
 
-    return parseInt(breakpointStr.replace(/\D/g, ''));
-  };
+  return parseInt(breakpointStr.replace(/\D/g, ''));
+};
 
 export default useWindowBreakpoint;
