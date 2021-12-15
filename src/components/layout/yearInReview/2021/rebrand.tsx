0d2d3e61e9d245aee71d825d0aa6b1a3230c4ae7
davidvanbeveren <david.vanbeveren@gmail.com>
2021-12-15 15:45:27 +0100

    Update rebrand.tsx

--- src/components/layout/yearInReview/2021/rebrand.tsx
+++ src/components/layout/yearInReview/2021/rebrand.tsx
@@ -24,17 +24,18 @@ const Rebrand: React.FC<RebrandProps> = ({ kate }) => {
           <div className="text-2xl md:text-left mx-auto md:w-2/3 space-y-5">
             <SectionHeader header={['Our', 'rebrand']} />
             <p>
-              This year, we decided to completely and professionally rebrand
-              both the organization and our website from the ground up. We
-              wanted our website and branding to still deeply represent our tech
-              background, but also be far more inviting and fun to better
-              represent our work and our community.
+            This year, we decided to professionally rebrand both the organization 
+            and our website from the ground up. We wanted our branding to still 
+            deeply represent our tech background, but also be far more inviting 
+            and fun to better represent our work and our community.
             </p>
             <p>
-              We not only re-branded our website, but also our social media -
-              and saw an immediate increase in applications of interested
-              volunteers for those who found us on either platform. Our branding
-              was donw by our newest Team Lead,{' '}
+            We saw an immediate increase in applications on both our website and 
+            social platforms from interested volunteers for those who found us 
+            on either platform.
+            </p>
+            <p>
+              Our branding was done by our newest Team Lead,{' '}
               <Link
                 href={{
                   pathname: '/blog/[slug]',
@@ -50,7 +51,7 @@ const Rebrand: React.FC<RebrandProps> = ({ kate }) => {
                 </a>
               </Link>{' '}
               - who&apos;s been at the heart of transforming the way we work and
-              look at design here at the Vegan Hacktivists.
+              look at design here at VH.
             </p>
             <div className="w-min pt-5 mx-auto md:mx-0">
               <LightButton href="/resources/VH_BrandGuide.pdf" className="my-5">
