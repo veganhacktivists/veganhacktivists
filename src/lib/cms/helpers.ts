5740fd03682026450d8c96b0f76b126c8b942855
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-01-12 14:25:28 +0100

    Fix blog ordering

--- src/lib/cms/helpers.ts
+++ src/lib/cms/helpers.ts
@@ -7,27 +7,29 @@ import type {
   ITeamFields,
 } from '../../types/generated/contentful';
 
-export const getBlogPreviewBySlug: (slug: string) => Promise<IBlogEntry> =
-  async (slug) => {
-    const response = await previewClient.getEntries({
-      'fields.slug': slug,
-      content_type: 'blogEntry',
-    });
+export const getBlogPreviewBySlug: (
+  slug: string
+) => Promise<IBlogEntry> = async (slug) => {
+  const response = await previewClient.getEntries({
+    'fields.slug': slug,
+    content_type: 'blogEntry',
+  });
 
-    return response.items[0] as IBlogEntry;
-  };
+  return response.items[0] as IBlogEntry;
+};
 
-export const getAllBlogSlugs: () => Promise<IBlogEntry['fields']['slug'][]> =
-  async () => {
-    const entries = await client.getEntries({
-      content_type: 'blogEntry',
-      select: 'fields.slug',
-    });
+export const getAllBlogSlugs: () => Promise<
+  IBlogEntry['fields']['slug'][]
+> = async () => {
+  const entries = await client.getEntries({
+    content_type: 'blogEntry',
+    select: 'fields.slug',
+  });
 
-    return (entries.items as Entry<IBlogEntryFields>[]).map(
-      (entry) => entry.fields.slug
-    );
-  };
+  return (entries.items as Entry<IBlogEntryFields>[]).map(
+    (entry) => entry.fields.slug
+  );
+};
 
 export const getActiveTeams: () => Promise<Entry<ITeamFields>[]> = async () => {
   const teams = await getContents<ITeamFields>({
@@ -53,21 +55,22 @@ export const getProjects: () => Promise<Entry<IProjectFields>[]> = async () => {
   return projects;
 };
 
-export const getFeaturedProjects: () => Promise<Entry<IProjectFields>[]> =
-  async () => {
-    const projects = await getContents<IProjectFields>({
-      contentType: 'project',
-      query: {
-        isFeatured: true,
-      },
-      other: {
-        order: '-fields.date',
-        limit: 8,
-      },
-    });
+export const getFeaturedProjects: () => Promise<
+  Entry<IProjectFields>[]
+> = async () => {
+  const projects = await getContents<IProjectFields>({
+    contentType: 'project',
+    query: {
+      isFeatured: true,
+    },
+    other: {
+      order: '-fields.date',
+      limit: 8,
+    },
+  });
 
-    return projects;
-  };
+  return projects;
+};
 
 export const getBlogEntries: (
   limit?: number
@@ -75,20 +78,7 @@ export const getBlogEntries: (
   const fields =
     'sys.createdAt,fields.publishDate,fields.featuredImage,fields.title,fields.slug,fields.excerpt,fields.tags';
 
-  const [newBlogs, oldBlogs] = await Promise.all([
-    getContents<IBlogEntryFields>({
-      contentType: 'blogEntry',
-      query: {
-        filters: {
-          exists: { publishDate: false },
-        },
-      },
-      other: {
-        order: '-sys.createdAt',
-        select: fields,
-        limit,
-      },
-    }),
+  const [dateBlogs, noDateBlogs] = await Promise.all([
     getContents<IBlogEntryFields>({
       contentType: 'blogEntry',
       query: {
@@ -102,15 +92,29 @@ export const getBlogEntries: (
         limit,
       },
     }),
+    getContents<IBlogEntryFields>({
+      contentType: 'blogEntry',
+      query: {
+        filters: {
+          exists: { publishDate: false },
+        },
+      },
+      other: {
+        order: '-sys.createdAt',
+        select: fields,
+        limit,
+      },
+    }),
   ]);
 
-  return [...newBlogs, ...oldBlogs].slice(0, limit);
-};
+  const allBlogs = [...dateBlogs, ...noDateBlogs];
 
-// export const getlastBlogEntries: (limit?: number) => Promise<IBlogEntry[]> =
-//   async (limit = 3) => {
-//     const lastEntries = await getContents<IBlogEntryFields>({
-//       contentType: 'blogEntry',
-//       other: { limit, sort:"" },
-//     });
-//   };
+  allBlogs.sort((a, b) => {
+    const dateA = new Date(a.fields.publishDate || a.sys.createdAt);
+    const dateB = new Date(b.fields.publishDate || b.sys.createdAt);
+
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  return allBlogs.slice(0, limit);
+};
