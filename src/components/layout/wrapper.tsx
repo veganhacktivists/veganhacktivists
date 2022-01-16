ee7b8b453305b19c06cbbcc99f2ffb60623f16c8
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-01-16 16:24:04 +0100

    Set JumpToContent button as first element in page

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -1,13 +1,5 @@
 import CookiesCTA from '../cookiesCTA';
 
-const PageWrapper: React.FC = ({ children }) => {
-  return (
-    <div className="flex flex-col justify-between min-h-screen w-full">
-      {children}
-    </div>
-  );
-};
-
 // http://web-accessibility.carnegiemuseums.org/code/skip-link/
 const JumpToContent: React.FC = () => {
   return (
@@ -15,17 +7,26 @@ const JumpToContent: React.FC = () => {
       role="button"
       className="text-white absolute top-0 text-center left-1/2 -translate-x-1/2 sm:left-2/3 xl:left-1/3 px-3 py-2 -translate-y-full md:focus:translate-y-0 focus:translate-y-24 z-30 transition-transform duration-200 rounded-md border-2"
       href="#main"
-      tabIndex={10}
     >
       Jump to content
     </a>
   );
 };
 
-export const MainWrapper: React.FC = ({ children }) => {
+const PageWrapper: React.FC = ({ children }) => {
   return (
     <>
       <JumpToContent />
+      <div className="flex flex-col justify-between min-h-screen w-full">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export const MainWrapper: React.FC = ({ children }) => {
+  return (
+    <>
       <main id="main" className="text-center min-h-[40rem]" tabIndex={-1}>
         {children}
         <CookiesCTA />
