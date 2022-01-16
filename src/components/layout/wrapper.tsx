704c2943b3213214283b6a5eefead954b34e029d
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-01-16 16:40:21 +0100

    Remove double border from Jump to content button

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -5,7 +5,7 @@ const JumpToContent: React.FC = () => {
   return (
     <a
       role="button"
-      className="text-white absolute top-0 text-center left-1/2 -translate-x-1/2 sm:left-2/3 xl:left-1/3 px-3 py-2 -translate-y-full md:focus:translate-y-0 focus:translate-y-24 z-30 transition-transform duration-200 rounded-md border-2"
+      className="text-white absolute top-0 text-center left-1/2 -translate-x-1/2 sm:left-2/3 xl:left-1/3 px-3 py-2 -translate-y-full focus:translate-y-22 md:focus:translate-y-2 z-30 transition-transform rounded-md"
       href="#main"
     >
       Jump to content
