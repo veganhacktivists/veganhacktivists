d58a19c32bdd29484644de063eea5c4a3dfde8ed
Ben Bowers <benjambowers@gmail.com>
2022-02-11 23:58:53 -0500

    Added new envelope image

--- src/images/separators.ts
+++ src/images/separators.ts
@@ -2,5 +2,6 @@ import pixelHeart from '../../public/images/VH_PixelHeart.png';
 import pixelFlower from '../../public/images/VH_PixelFlower.png';
 import pixelStar from '../../public/images/VH_PixelStar.png';
 import pixelPig from '../../public/images/VH_PixelPig.png';
+import pixelEnvelope from '../../public/images/VH_PixelEnvelope.png';
 
-export { pixelHeart, pixelFlower, pixelStar, pixelPig };
+export { pixelHeart, pixelFlower, pixelStar, pixelPig, pixelEnvelope };
