c370eca44bee5339f467d9453d4c5a9cc826e9b5
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-06-18 13:51:43 +0200

    Add seed script to DB

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,13 +1,44 @@
-import type { Post } from '@prisma/client';
-import type { GetServerSideProps, NextPage } from 'next';
+import type { User } from '@prisma/client';
+import type { InferGetServerSidePropsType, NextPage } from 'next';
 import prisma from '../../lib/db/prisma';
 
-interface PlaygroundProps {
-  posts: Post[];
-}
+type PlaygroundProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
-export const getServerSideProps: GetServerSideProps = async ({}) => {
-  const posts = await prisma.post.findMany();
+const ShowUser: React.FC<Pick<User, 'email'>> = ({ email }) => {
+  return <div>{email}</div>;
+};
+
+const ShowPost: React.FC<PlaygroundProps['posts'][0]> = ({
+  title,
+  body,
+  user,
+  _count: { votes },
+}) => {
+  return (
+    <div className="border border-black w-1/2 mx-auto">
+      <div>{title}</div>
+      <div>{body}</div>
+      <div>Number of votes: {votes}</div>
+      <div className="mt-5">
+        <div>User</div>
+        <ShowUser {...user} />
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  // TODO: find out how to work with dates without all this boilerplate
+  const posts = await prisma.post.findMany({
+    include: {
+      user: {
+        select: {
+          email: true,
+        },
+      },
+      _count: { select: { votes: true } },
+    },
+  });
 
   return {
     props: {
@@ -19,12 +50,14 @@ export const getServerSideProps: GetServerSideProps = async ({}) => {
 const Playground: NextPage<PlaygroundProps> = ({ posts }) => {
   return (
     <div>
-      <div className="text-xl font-bold">Posts:</div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{JSON.stringify(post)}</li>
-        ))}
-      </ul>
+      <div>
+        <div className="text-xl font-bold">Posts:</div>
+        <div>
+          {posts.map((post) => (
+            <ShowPost key={post.id} {...post} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
