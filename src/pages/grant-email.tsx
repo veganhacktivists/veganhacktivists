b88cc5b4778db1b5a138cfd0cc9898bdf5f378ca
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-06-18 10:46:15 +0200

    Add mail template page for preview

---
+++ src/pages/grant-email.tsx
@@ -0,0 +1,7 @@
+import GrantsEmailTemplate from '../components/layout/grants/emailTemplate';
+
+const GrantEmail: React.FC = () => {
+  return <GrantsEmailTemplate />;
+};
+
+export default GrantEmail;
