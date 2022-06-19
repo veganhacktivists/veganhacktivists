e4a177e267b25cd99ff84384fad6e1987a7c4c28
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-06-20 00:29:12 +0200

    Add email magic link auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,27 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
-import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '../../../lib/db/prisma';
+import { OUR_EMAIL } from '../../../lib/mail';
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    // GithubProvider({}),
-    EmailProvider({}),
+    EmailProvider({
+      name: 'magic link',
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: OUR_EMAIL,
+    }),
   ],
   pages: {
     signIn: '/auth/signin',
+    verifyRequest: '/auth/verify-request',
   },
 });
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,5 +1,8 @@
 import type { InferGetServerSidePropsType, NextPage } from 'next';
-import { getProviders, signIn } from 'next-auth/react';
+import { getProviders, signIn, signOut, useSession } from 'next-auth/react';
+import { useState, useTransition } from 'react';
+import { DarkButton } from '../../components/decoration/buttons';
+import TextInput from '../../components/forms/inputs/textInput';
 
 type SignInProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
@@ -12,17 +15,57 @@ export const getServerSideProps = async () => {
 };
 
 const SignIn: NextPage<SignInProps> = ({ providers }) => {
+  const session = useSession();
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   if (!providers) return null;
 
   return (
-    <div>
+    <div className="bg-grey-background p-10">
+      {JSON.stringify(session)}
       {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
+        <div key={provider.id}>
           <button onClick={() => signIn(provider.id)}>
             Sign in with {provider.name}
           </button>
         </div>
-      ))}{' '}
+      ))}
+      {session.status === 'authenticated' && (
+        <DarkButton
+          disabled={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            await signOut();
+          }}
+        >
+          Sign out!
+        </DarkButton>
+      )}
+      {session.status === 'unauthenticated' && (
+        <div className="">
+          <div className="w-1/2 mx-auto">
+            <TextInput
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.currentTarget.value);
+              }}
+            />
+          </div>
+          <DarkButton
+            disabled={isLoading}
+            onClick={async () => {
+              setIsLoading(true);
+              await signIn('email', { email });
+            }}
+          >
+            Sign in!
+          </DarkButton>
+        </div>
+      )}
     </div>
   );
 };
