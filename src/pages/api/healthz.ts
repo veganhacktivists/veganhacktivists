006b2ad093047665d9577ca37512a291b7571910
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-07-17 21:54:37 +0200

    Add log and DB connection check to health API

--- src/pages/api/healthz.ts
+++ src/pages/api/healthz.ts
@@ -1,7 +1,20 @@
+/* eslint-disable no-console */
 import type { NextApiHandler } from 'next';
+import HttpCodes from 'http-status-codes';
+import prisma from '../../lib/db/prisma';
 
-const handler: NextApiHandler = (_req, res) => {
-  return res.status(200).json({ status: 'ok' });
+const handler: NextApiHandler = async (_req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1;`;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    console.error('Health check FAIL', e.message || e);
+    return res
+      .status(HttpCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: e.message || e });
+  }
+  console.info('Health check OK');
+  return res.status(HttpCodes.OK).end();
 };
 
 export default handler;
