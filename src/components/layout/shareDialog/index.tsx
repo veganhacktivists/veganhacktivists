0d87ccff0671de5068e9494c9eae37a5da1d3d3d
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-07-23 17:10:57 +0200

    Make share dialog more aesthetic

--- src/components/layout/shareDialog/index.tsx
+++ src/components/layout/shareDialog/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import Modal from '../modal';
-import CustomImage from '../../decoration/customImage';
+
 import { pixelStar } from '../../../images/separators';
+import CustomImage from '../../decoration/customImage';
+import SquareField from '../../decoration/squares';
+import { SectionHeader } from '../../decoration/textBlocks';
+import ContentfulImage from '../contentfulImage';
+import Modal from '../modal';
 import {
   CopyButton,
   EmailButton,
@@ -12,10 +16,8 @@ import {
   TwitterButton,
   WhatsappButton,
 } from './shareButtons';
+
 import type ShareInfo from './shareInfo';
-import { SectionHeader } from '../../decoration/textBlocks';
-import ContentfulImage from '../contentfulImage';
-import SquareField from '../../decoration/squares';
 
 /**
  * Proptypes of `ShareDialog`.
@@ -38,12 +40,7 @@ const ShareDialog: React.FC<ShareDialogProps> = ({
   onClose,
 }) => {
   return (
-    <Modal
-      isOpen={open}
-      onClose={() => {
-        onClose();
-      }}
-    >
+    <Modal isOpen={open} onClose={onClose}>
       <>
         <SquareField
           squares={[
@@ -67,24 +64,26 @@ const ShareDialog: React.FC<ShareDialogProps> = ({
                 header={['Share', 'this project']}
               >
                 <div className="-mt-4 text-3xl">{shareInfo.title}</div>
-                <div className="w-1/2 mx-auto mt-3">
-                  <ContentfulImage
-                    image={shareInfo.image}
-                    alt={`${shareInfo.title} logo`}
-                  />
-                </div>
               </SectionHeader>
             </span>
           </h1>
-          <div className="grid grid-cols-4 mx-auto align-middle gap-x-2 gap-y-8 place-items-center auto-cols-min md:w-3/4">
-            <EmailButton onClick={onClose} shareInfo={shareInfo} />
-            <FacebookButton onClick={onClose} shareInfo={shareInfo} />
-            <TwitterButton onClick={onClose} shareInfo={shareInfo} />
-            <LinkedinButton onClick={onClose} shareInfo={shareInfo} />
-            <WhatsappButton onClick={onClose} shareInfo={shareInfo} />
-            <TelegramButton onClick={onClose} shareInfo={shareInfo} />
-            <RedditButton onClick={onClose} shareInfo={shareInfo} />
-            <CopyButton onClick={onClose} shareInfo={shareInfo} />
+          <div className="flex flex-col justify-center gap-5 mx-auto mb-10 md:flex-row">
+            <div className="w-1/2 mx-auto md:w-1/3">
+              <ContentfulImage
+                image={shareInfo.image}
+                alt={`${shareInfo.title} logo`}
+              />
+            </div>
+            <div className="grid grid-cols-4 gap-4 align-middle gap-y-8 place-items-center auto-cols-min">
+              <EmailButton onClick={onClose} shareInfo={shareInfo} />
+              <FacebookButton onClick={onClose} shareInfo={shareInfo} />
+              <TwitterButton onClick={onClose} shareInfo={shareInfo} />
+              <LinkedinButton onClick={onClose} shareInfo={shareInfo} />
+              <WhatsappButton onClick={onClose} shareInfo={shareInfo} />
+              <TelegramButton onClick={onClose} shareInfo={shareInfo} />
+              <RedditButton onClick={onClose} shareInfo={shareInfo} />
+              <CopyButton onClick={onClose} shareInfo={shareInfo} />
+            </div>
           </div>
         </div>
       </>
