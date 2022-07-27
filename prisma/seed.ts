099518edc33e2ad7dd6b8814ca3994b19eb42348
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-07-27 07:36:42 +0200

    Create sample playground requests in seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,71 @@
 /* eslint-disable no-console */
-import type { User } from '@prisma/client';
-import { PrismaClient } from '@prisma/client';
+import { Priority, PrismaClient } from '@prisma/client';
 
 import { faker } from '@faker-js/faker';
 
+import type { Prisma } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
-const USER_NUMBER = 50;
+const NUMBER = 50;
 
-const users: Pick<User, 'email'>[] = Array(USER_NUMBER)
-  .fill(null)
-  .map(() => ({ email: faker.internet.email() }));
+const seedUsers = async (n: number = NUMBER) => {
+  const users: Prisma.UserCreateManyInput[] = Array(n)
+    .fill(null)
+    .map(() => ({
+      email: faker.internet.email(),
+      name: faker.name.findName(),
+    }));
+  const { count } = await prisma.user.createMany({ data: users });
+  console.log('Seeded', count, 'users');
+};
+
+const seedRequests = async (n: number = NUMBER) => {
+  const users = await prisma.user.findMany();
+  const requests: Prisma.PlaygroundRequestCreateManyInput[] = Array(n)
+    .fill(null)
+    .map(() => {
+      return {
+        budget: faker.datatype.boolean()
+          ? faker.datatype.float({
+              min: 10,
+              max: 10000,
+              precision: faker.helpers.arrayElement([0.01, 0.1]),
+            })
+          : faker.datatype.number({ min: 10, max: 10000 }),
+        category: faker.hacker.noun(),
+        estimatedTimeDays: faker.datatype.number({ min: 1, max: 30 }),
+        description: faker.hacker.phrase() + faker.lorem.paragraph(),
+        dueDate: faker.date.future(),
+        free: faker.datatype.boolean(),
+        priority: faker.helpers.objectValue(Priority),
+        roleTitle: faker.hacker.noun(),
+        requiredSkills: faker.helpers.uniqueArray(
+          () => faker.hacker.ingverb(),
+          faker.datatype.number({ min: 0, max: 10 })
+        ),
+        website: faker.internet.url(),
+        title: faker.hacker.phrase(),
+        requesterId: faker.helpers.arrayElement(users).id,
+        phone: faker.phone.number(),
+        organization: faker.company.companyName(),
+      };
+    });
+  const { count } = await prisma.playgroundRequest.createMany({
+    data: requests,
+  });
+  console.log('Seeded', count, 'requests');
+};
+
+const cleanup = async () => {
+  await prisma.playgroundRequest.deleteMany();
+  await prisma.user.deleteMany();
+};
 
 async function main() {
-  await prisma.user.deleteMany();
-
-  await prisma.user.createMany({ data: users });
-  const newUsers = await prisma.user.findMany();
-  console.log('Seeded users', newUsers);
+  await cleanup();
+  await seedUsers();
+  await seedRequests();
 }
 
 main()
