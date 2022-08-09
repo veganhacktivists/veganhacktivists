ca513ec4766802a9566b5363049a70f60639fb4f
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-08-09 17:53:12 +0200

    Add `PlaygroundLayout` hero images and taglines

--- src/components/layout/playground/layout.tsx
+++ src/components/layout/playground/layout.tsx
@@ -6,27 +6,54 @@ import { NextSeo } from 'next-seo';
 import { useSession } from 'next-auth/react';
 import React from 'react';
 
-import heroImage from '../../../../public/images/playground/hero.jpg';
-import tagline from '../../../../public/images/playground/getsupportfrom.svg';
+import heroFishImage from '../../../../public/images/playground/fishHero.jpg';
+import heroCrabImage from '../../../../public/images/playground/crabHero.jpg';
+import submitTagline from '../../../../public/images/playground/getsupport.svg';
+import volunteerTagline from '../../../../public/images/playground/volunteer.svg';
+
+import SquareField from '../../decoration/squares';
 
 import Hero from 'components/decoration/hero';
 import { DarkButton, OutlineButton } from 'components/decoration/buttons';
 
 import type { Layout } from 'types/persistentLayout';
 
+const HERO_DECORATION_SQUARES = [
+  { color: 'white', size: 16, left: 0, bottom: 0 },
+  { color: 'orange', size: 16, left: 0, top: 0 },
+  { color: 'pink', size: 24, left: 16, bottom: 0 },
+  { color: 'yellow', size: 32, right: 0, top: -16 },
+  { color: 'yellow-orange', size: 16, right: 32, bottom: 16 },
+  { color: 'white', size: 16, right: 32, bottom: 0 },
+];
+
 const PlaygroundLayout: Layout = ({ children }) => {
+  const router = useRouter();
+  const isSupportPage =
+    router.pathname === '/playground' ||
+    router.pathname === '/playground/submit';
+
   // TODO: put the real tagline here, stop messing up the padding
   return (
     <>
       <NextSeo titleTemplate="%s | VH Playground" />
-      <div className="">
+      <div>
         <Hero
-          imageBackground={heroImage}
+          imageBackground={isSupportPage ? heroFishImage : heroCrabImage}
           tagline={{
-            alt: 'Get Support from Playground',
-            image: tagline,
+            alt: isSupportPage
+              ? 'Get Support from Playground'
+              : 'Volunteer for the Animals',
+            image: isSupportPage ? submitTagline : volunteerTagline,
           }}
           alignment="left"
+          classNameMapping={{
+            container: 'bg-center',
+          }}
+        />
+        <SquareField
+          squares={HERO_DECORATION_SQUARES}
+          className="hidden md:block"
         />
       </div>
       {children}
