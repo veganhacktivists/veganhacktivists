a4450e098545a91830211f06a191c38ab3ecba75
Gerard O'Neill <grardb@gmail.com>
2022-08-14 23:30:18 +0200

    Add keywords for Team Strawberry

--- src/lib/mail/router/index.ts
+++ src/lib/mail/router/index.ts
@@ -5,9 +5,17 @@ export const OUR_EMAIL = process.env.EMAIL_TO!;
 const projectSpecificEmails: Record<string, (string | string[])[]> = {
   [process.env.EMAIL_TO_AVOCADO!]: [],
   [process.env.EMAIL_TO_BANANA!]: [['vegan', 'bootcamp']],
-  [process.env.EMAIL_TO_CARROT!]: ['5m5v', ['5', 'minutes', 'vegans']],
+  [process.env.EMAIL_TO_CARROT!]: ['5m5v', ['minutes', 'vegans']],
   [process.env.EMAIL_TO_CHEATSHEET!]: ['cheatsheet'],
-  [process.env.EMAIL_TO_SWEET_POTATO!]: [['vegan', 'linguist']],
+  [process.env.EMAIL_TO_STRAWBERRY!]: [
+    'animalsupportbot',
+    'veganactivismbot',
+    ['animal', 'support', 'bot'],
+    ['artificial', 'intellegence'],
+    ['machine', 'learning'],
+    ['vegan', 'activism', 'bot'],
+  ],
+  [process.env.EMAIL_TO_SWEET_POTATO!]: [['vegan', 'linguists']],
   [process.env.EMAIL_TO_WATERMELON!]: ['activist hub'],
 };
 
