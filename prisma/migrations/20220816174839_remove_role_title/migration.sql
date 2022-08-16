b7cc3223996e98fa23c83c6120788c1a7b795d23
Joaquín Triñanes <joaquin@veganhacktivists.org>
2022-08-16 19:35:49 +0200

    Make budget optional

---
+++ prisma/migrations/20220816174839_remove_role_title/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `roleTitle` on the `PlaygroundRequest` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PlaygroundRequest" DROP COLUMN "roleTitle";
