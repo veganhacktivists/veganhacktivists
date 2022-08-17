fd24d6841bfb877360bcc94c8406a0145bb49da0
David van Beveren <admin@veganhacktivists.org>
2022-08-17 02:19:16 +0200

    Better login email message

--- src/pages/auth/verify-request.tsx
+++ src/pages/auth/verify-request.tsx
@@ -8,9 +8,9 @@ const VerifyRequest: NextPage = () => {
   const { back } = useRouter();
   return (
     <div>
-      <h1 className="font-bold text-3xl">Check your email</h1>
+      <h1 className="font-bold text-3xl mt-8">Check your email</h1>
 
-      <div className="text-xl">
+      <div className="text-xl mt-4">
         A sign in link has been sent to your email address.
       </div>
       <div>
