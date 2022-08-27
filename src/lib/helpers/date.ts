2c8194764bfebeb8c229bc300e6170ea94451ed9
Headscracher <t.frohme@all4-one.de>
2022-08-28 00:00:41 +0200

    Add rounding for human readable time diff to fix diff issues

--- src/lib/helpers/date.ts
+++ src/lib/helpers/date.ts
@@ -6,13 +6,36 @@ export const readableTimeDiff = (date: Date) => {
 
   const isPastDate = other.diff(now).milliseconds < 0;
 
-  const timeDiff = (isPastDate ? now : other)
+  const timeDiff: {
+    years?: number;
+    months?: number;
+    weeks?: number;
+    days?: number;
+  } = (isPastDate ? now : other)
     .diff(isPastDate ? other : now, ['years', 'months', 'weeks', 'days'])
     .normalize()
     .toObject();
 
+  let roundedDiff: {
+    years?: number;
+    months?: number;
+    weeks?: number;
+    days?: number;
+  } = {};
+
+  Object.keys(timeDiff).forEach((keystring) => {
+    const key = keystring as keyof typeof roundedDiff;
+    const original = timeDiff[key];
+    if (typeof original === 'number') {
+      const addition = {
+        [key]: Math.round(original),
+      };
+      roundedDiff = { ...roundedDiff, ...addition };
+    }
+  });
+
   const diffWithoutZeroes = Object.fromEntries(
-    Object.entries(timeDiff).filter(([, value]) => value >= 0.5)
+    Object.entries(roundedDiff).filter(([, value]) => value >= 1)
   );
 
   if (Object.keys(diffWithoutZeroes).length === 0) {
