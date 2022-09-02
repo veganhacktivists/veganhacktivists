c365530da48d02d79a48c979ffd6a0628bcaa851
Headscracher <t.frohme@all4-one.de>
2022-09-02 22:38:10 +0200

    add package mjml + change login email design
    The mjml package is needed to generate a responsive html email by using different components

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import mjml2html from 'mjml';
 
 import emailClient, { OUR_EMAIL_FORMATTED } from 'lib/mail';
 import prisma from 'lib/db/prisma';
@@ -25,40 +26,28 @@ const sendVerificationRequest = async (
 function verificationHtml(params: { url: string; host: string; theme: Theme }) {
   const { url, host, theme } = params;
 
-  return `
-<body style="background: lightgray">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: gray; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif;">
-        Sign in to <strong>${host}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" ><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif;">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`;
+  return mjml2html(`
+<mjml>
+    <mj-body background-color="#161919">
+    <mj-section background-color="#161919">
+      <mj-image alt="Vegan Hacktivists" src="https://veganhacktivists.org/images/VH-logo-web-white.png"></mj-image>
+    </mj-section>
+    <mj-section background-color="#ffffff">
+    <mj-column>
+    <mj-text align="left" font-weight="bold" font-family="-apple-system,BlinkMacSystemFont,Segoe UI, Roboto, Helvetica, Arial" font-size="16px" color="#161919">Hey there!</mj-text>
+    <mj-text align="left" font-family="-apple-system,BlinkMacSystemFont,Segoe UI, Roboto, Helvetica, Arial" font-size="16px" color="#161919">Someone tried to login to ${host} with your email address.<br>If this was you, you're able to login here:</mj-text>
+    <mj-button border-left="10px solid #64BC46" background-color="#292929" href="${url}" border-radius="0px" font-family="Rajdhani, monospace" font-size="18px" color="#ffffff">Login</mj-button>
+    <mj-text align="left" font-family="-apple-system,BlinkMacSystemFont,Segoe UI, Roboto, Helvetica, Arial" font-size="16px" color="#161919">Otherwise you can safely ignore this mail.</mj-texta>
+</mj-column>
+    </mj-section>
+<mj-section>
+<mj-text align="center" font-family="-apple-system,BlinkMacSystemFont,Segoe UI, Roboto, Helvetica, Arial" color="#ffffff">© 2022 Vegan Hacktivists. All rights reserved.</mj-text>
+</mj-section>
+</mj-body>
+</mjml>
+  `)?.html;
 }
 
-/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
 function text({ url, host }: { url: string; host: string }) {
   return `Sign in to ${host}\n${url}\n\n`;
 }
