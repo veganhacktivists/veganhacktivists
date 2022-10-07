eda05b3eb0e0d28233eb61333e595e5f2d6e5351
Headscracher <t.frohme@all4-one.de>
2022-10-07 16:48:41 +0200

    Remove DataDashboardValueId from db struct

---
+++ prisma/migrations/20221007144817_remove_data_dashboard_value_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dataDashboardValueId` on the `DataDashboardData` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DataDashboardData" DROP COLUMN "dataDashboardValueId";
