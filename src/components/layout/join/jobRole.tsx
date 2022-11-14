27082a03e87599a20808392367eba4f981af9772
David Price <davidwillprice@gmail.com>
2022-11-14 18:07:15 +0000

    Job Role Visual Improvements - Fix 'Apply Now' button being hidden on small mobile devices - Fix headings not having x margins - Fix text getting crushed on small mobile devices - Fix squares covering the job icons on mobile devices

--- src/components/layout/join/jobRole.tsx
+++ src/components/layout/join/jobRole.tsx
@@ -28,22 +28,21 @@ const JobRole: React.FC<JobRoleProps> = ({
 
   return (
     <div className="bg-gray-background text-grey-dark">
-      <SquareField
-        squares={[{ color: squareColor, size: 16, right: 0 }]}
-        className=""
-      />
-      <div
-        style={{ backgroundColor }}
-        className="md:h-52 flex flex-col justify-center py-8"
-      >
-        <CustomImage alt={title} src={image} objectFit="contain" />
+      <div style={{ backgroundColor }}>
+        <SquareField
+          squares={[{ color: squareColor, size: 16, right: 0 }]}
+          className=""
+        />
+        <div className="md:h-52 flex flex-col justify-center py-8 relative z-20">
+          <CustomImage alt={title} src={image} objectFit="contain" />
+        </div>
       </div>
-      <h2 className="text-4xl font-modo font-bold my-8 md:px-8">{title}</h2>
-      <p className="px-10 md:px-8 text-2xl text-center">{description}</p>
+      <h2 className="text-4xl font-modo font-bold my-8 px-5">{title}</h2>
+      <p className="px-5 text-2xl text-center">{description}</p>
       <div className="my-8 w-2/3 mx-auto">
         <LightButton
           href={href}
-          className="font-semibold font-mono px-16 py-2 my-4"
+          className="font-semibold font-mono px-8 py-2 my-4"
         >
           Apply Now
         </LightButton>
