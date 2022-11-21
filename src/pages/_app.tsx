d3d5687fb414247d0ec09fe31fd1ab2beb9659e5
David Price <davidwillprice@gmail.com>
2022-11-21 16:05:34 +0000

    Localise Google Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import { trpc } from 'lib/client/trpc';
 
 import 'tailwindcss/tailwind.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import '../styles/fonts.css';
 import type { NextPage } from 'next';
 import type { DefaultSeoProps } from 'next-seo';
 import type ReactAxe from '@axe-core/react';
