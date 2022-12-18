daf24355cceb822abba37dc8a854252b78af3174
David Price <45039949+davidwillprice@users.noreply.github.com>
2022-12-18 13:19:15 +0000

    Refactor  "Read More / apply" Button to Avoid Unnecessary HTML Comments
    Co-authored-by: Headscracher <t.frohme@all4-one.de>

--- src/components/layout/playground/requests/requestCard.tsx
+++ src/components/layout/playground/requests/requestCard.tsx
@@ -177,11 +177,12 @@ const PlaygroundRequestCard: React.FC<
               canEdit ? 'w-1/2' : ''
             }`}
           >
-            Read more{' '}
-            {session?.user?.role !== 'Admin' &&
-            requester?.id !== session?.user?.id
-              ? '/ apply'
-              : null}
+                        {`Read more${
+              session?.user?.role !== 'Admin' &&
+              requester?.id !== session?.user?.id
+                ? ' / apply'
+                : ''
+            }`}
           </DarkButton>
           {canEdit && (
             <GreyButton
