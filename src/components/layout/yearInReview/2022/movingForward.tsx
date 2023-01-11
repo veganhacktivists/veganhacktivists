dbb600dd87b7c27505428d7f36989f0133b00702
David Price <davidwillprice@gmail.com>
2023-01-11 21:12:21 +0000

    Update 'Forward & Onward' For 2022

--- src/components/layout/yearInReview/2022/movingForward.tsx
+++ src/components/layout/yearInReview/2022/movingForward.tsx
@@ -15,35 +15,29 @@ const MovingForward: React.FC = () => {
         ]}
       />
       <SectionContainer color="grey-darker" className="text-white">
-        <div className="text-center md:w-1/2 mx-auto space-y-5">
+        <div className="text-center md:w-3/4 mx-auto space-y-5">
           <div>
-            <div className="w-24 mx-auto">
+            <div className="w-24 mx-auto mb-6">
               <CustomImage src={pixelPig} alt="" />
             </div>
-
-            <h2 className="font-bold text-4xl pt-5">Moving forward</h2>
+            <h2 className="text-6xl font-bold font-mono uppercase">
+              Forward & Onward
+            </h2>
           </div>
           <p>
-            What a year! We wanted to focus on data-based efforts while staying
-            true to our spirit of experimental projects, and we were able to
-            achieve those goals through Activist Hub and Vegan Linguists. This
-            has truly been the best year for creating connections and
-            partnerships in the animal protection movement, and we are extremely
-            pleased with the work we&apos;ve accomplished.
-          </p>
-          <p>
-            This year, we have also prioritized organizing and optimizing
-            internal processes. This was crucial for us since a core focus of
-            2022 is to expand our volunteer network to meet the demand of our
-            services. We want to continue being able to serve the multifaceted
-            needs of the vegan movement and empower people to become activists.
-          </p>
-          <p>
-            We&apos;re really excited to hear your thoughts in our 2021 year in
-            review, and if you like what we do, please consider supporting us by
-            clicking the button below. Your donation ensures that all of our
-            work and projects remain free and accessible to everyone, and we
-            can&apos;t begin to thank you enough for the support!
+            We witnessed our biggest transformation to date this year. We
+            focused significantly on the internal workings and processes of our
+            organization, while also doing our best to serve organizations and
+            advocates. We launched technology using machine learning and
+            artificial intelligence, and we delivered new features and upgrades
+            to high-impact projects. We attended conferences, spoke on podcasts,
+            and created meaningful connections to expand our reach and impact.
+            We launched the first study of its kind to collect data on the state
+            of tech in the movement, and we partnered with 10 unique
+            organizations in the animal protection movement on various
+            initiatives. There is so much that we’ve done behind the scenes, and
+            there is so much more to come. We are excited to be on this journey
+            with all of you.
           </p>
           <div className="w-min mx-auto pt-10 pb-10">
             <LightButton href="/support" className="w-min px-5">
