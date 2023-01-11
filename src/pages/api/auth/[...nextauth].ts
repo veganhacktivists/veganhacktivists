502124ffb4d562f4454f36d16d1731bb0bc90ce1
prpcodes <work@prp.codes>
2023-01-11 22:50:47 +0100

    add callbackUrl to identify the current page and send the right email according to it

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,10 @@ import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { verificationMail } from '../../../components/layout/mail/emailTemplates';
+import {
+  verificationMail,
+  verifyRequestEmail,
+} from '../../../components/layout/mail/emailTemplates';
 import { OUR_EMAIL_FROM_FORMATTED } from '../../../lib/mail/router';
 
 import emailClient from 'lib/mail';
@@ -15,13 +18,20 @@ const sendVerificationRequest = async (
   params: SendVerificationRequestParams
 ) => {
   const { identifier, url } = params;
-  const { host } = new URL(url);
+  const { searchParams } = new URL(url);
+  // the current url
+  const callbackUrl = searchParams.get('callbackUrl');
+
   await emailClient.sendMail({
     to: identifier,
     from: OUR_EMAIL_FROM_FORMATTED,
     subject: 'Vegan Hacktivists Playground login',
-    text: verificationMail(url, true),
-    html: verificationMail(url),
+    text: callbackUrl?.includes('signin')
+      ? verificationMail(url, true)
+      : verifyRequestEmail(url, true),
+    html: callbackUrl?.includes('signin')
+      ? verificationMail(url)
+      : verifyRequestEmail(url),
   });
 };
 
