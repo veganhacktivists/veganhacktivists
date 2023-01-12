c0a38d014d84f91c0c37d3a651d5580c1e9c3f81
Headscracher <t.frohme@all4-one.de>
2023-01-12 21:10:43 +0100

    Update sharing knowledge section

--- src/components/layout/yearInReview/2022/sharingKnowledgeAndSupport.tsx
+++ src/components/layout/yearInReview/2022/sharingKnowledgeAndSupport.tsx
@@ -22,12 +22,20 @@ const ImageWithCaption: React.FC<ImageWithCaptionTypes> = ({
   bgColor,
 }) => {
   return (
-    <div className={`bg-${bgColor}`}>
-      <CustomImage src={image} alt={caption} />
-      <div className={'w-full p-5 text-left'}>
-        <span className="font-bold">{caption}</span>
-        <br />
-        <span className="italic">{subcaption}</span>
+    <div className={'flex flex-col'}>
+      <CustomImage
+        src={image}
+        alt="Vegan Hacktivist team at the Animal & Vegan Advocacy Summit"
+      />
+      <div
+        className={'text-left flex-x-grow flex-x-shrink flex flex-col h-auto'}
+      >
+        <div className={`bg-${bgColor} items-center`}>
+          <div className="p-5">
+            <span className="font-bold block">{caption}</span>
+            <span className="italic">{subcaption}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -37,44 +45,47 @@ const SharingKnowledgeAndSupport: React.FC = () => {
   return (
     <>
       <SectionContainer
-        color="grey-background"
         header={
           <SectionHeader
-            className="text-grey-dark"
             header={['Sharing', 'knowledge & support']}
-          >
-            This year, we were invited to speak at CARE Conference in Warsaw,
-            Poland and the first-ever AVA Summit in Washington, DC. We spoke
-            about our work for the animal protection movement and how
-            organizations and individuals could get involved with our services
-            to get the support they need.
-          </SectionHeader>
+            newDesign={true}
+          />
         }
       >
-        <div className="flex flex-col justify-center mx-auto md:flex-row gap-x-24 gap-y-10 ">
-          <div className="flex flex-col lg:grid grid-rows-2 grid-cols-3 gap-5 md:w-3/4 max-w-[1350px] ">
-            <div className="row-span-1">
-              <ImageWithCaption
-                bgColor="yellow"
-                image={malina}
-                caption={'Malina Tran at CARE Conference'}
-                subcaption={'Data & Tech in Animal Rights Activism'}
-              />
+        <div className="text-xl md:w-2/3 xl:w-1/2 mx-auto">
+          This year, we were invited to speak at <b>CARE Conference</b> in
+          Warsaw, Poland and the first-ever <b>AVA Summit</b> in Washington, DC.
+          We spoke about our work for the animal protection movement and how
+          organizations and individuals could get involved with our services to
+          get the support they need.
+        </div>
+        <div className="xl:w-2/3 mx-auto mt-20">
+          <div className="flex flex-col md:flex-row gap-5 pb-20">
+            <div className="flex flex-col flex-1 min-w-[29%] xl:min-w-[31%] gap-5 justify-between">
+              <div className="flex ">
+                <ImageWithCaption
+                  bgColor="yellow"
+                  image={malina}
+                  caption={'Malina Tran at CARE Conference'}
+                  subcaption={'Data & Tech in Animal Rights Activism'}
+                />
+              </div>
+              <div className="flex ">
+                <ImageWithCaption
+                  bgColor="green"
+                  image={james}
+                  caption={'James Morgan at AVA Summit'}
+                  subcaption={'Tech Innovation in Animal Protection'}
+                />
+              </div>
             </div>
-            <div className="row-span-2 col-span-2">
-              <CustomImage
-                src={family}
-                alt="Vegan Hacktivist team at the Animal & Vegan Advocacy Summit"
-                className="w-full"
-              />
-            </div>
-            <div className="row-span-1 flex items-end xl:mb-[0.45rem]">
-              <ImageWithCaption
-                bgColor="green"
-                image={james}
-                caption={'James Morgan at AVA Summit'}
-                subcaption={'Tech Innovation in Animal Protection'}
-              />
+            <div className="flex  items-center">
+              <div className="flex">
+                <CustomImage
+                  src={family}
+                  alt="Vegan Hacktivist team at the Animal & Vegan Advocacy Summit"
+                />
+              </div>
             </div>
           </div>
         </div>
