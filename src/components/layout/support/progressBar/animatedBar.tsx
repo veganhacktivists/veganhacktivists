a0019ee4573863bdc0d0e0ba8fcfc08403704b15
David van Beveren <admin@veganhacktivists.org>
2023-01-23 14:46:41 +0700

    Switch Patreon goal to general support goal

--- src/components/layout/support/progressBar/animatedBar.tsx
+++ src/components/layout/support/progressBar/animatedBar.tsx
@@ -39,7 +39,7 @@ const AnimatedBar: React.FC<AnimatedBarProps> = ({ current, goal }) => {
               </animated.h1>
             </div>
             <p className="w-full text-lg text-left text-white md:text-xl">
-              Current monthly Patreon donations
+              Current monthly donations
             </p>
           </div>
         </animated.div>
