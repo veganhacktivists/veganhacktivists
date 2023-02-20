d576dc4a3df689ca4fcdefff7481b07c51b4bd5b
MatsThieme <mats.thieme.mt@gmail.com>
2023-02-20 23:08:40 +0100

    Keep phone optional in db

--- prisma/migrations/20221230170737_make_phone_mandatory_for_playground_requests/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `phone` on table `PlaygroundRequest` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "PlaygroundRequest" ALTER COLUMN "phone" SET NOT NULL;
