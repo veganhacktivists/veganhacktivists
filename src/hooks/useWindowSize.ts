aaf0ffd226ce53bee43de02374585d84e563afe9
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-05-20 00:32:19 +0200

    Adapt useWindowSize hook to use any element

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,31 +1,48 @@
 import { useState, useEffect } from 'react';
 
+import type { RefObject } from 'react';
+
 interface Size {
   width?: number;
   height?: number;
+  scroll: Omit<Size, 'scroll'>;
 }
 
-const useWindowSize: () => Size = () => {
+const useWindowSize = (elementRef?: RefObject<HTMLElement>): Size => {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState<Size>({
     width: undefined,
     height: undefined,
+    scroll: {
+      width: undefined,
+      height: undefined,
+    },
   });
 
   useEffect(() => {
+    const element = elementRef?.current ?? window;
     const handleResize = () => {
       setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width:
+          'innerWidth' in element ? element.innerWidth : element.clientWidth,
+        height:
+          'innerHeight' in element ? element.innerHeight : element.clientHeight,
+        scroll:
+          'scrollWidth' in element
+            ? {
+                width: element.scrollWidth,
+                height: element.scrollHeight,
+              }
+            : { width: undefined, height: undefined },
       });
     };
 
-    window.addEventListener('resize', handleResize);
+    element.addEventListener('resize', handleResize);
     handleResize();
 
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    return () => element.removeEventListener('resize', handleResize);
+  }, [elementRef]);
 
   return windowSize;
 };
