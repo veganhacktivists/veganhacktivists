ef8d1760b626f984ff2a03f7cc662d7abdecd4e1
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-05-25 13:34:01 +0200

    Add our comunities section

--- src/components/layout/work/ourCommunities.tsx
+++ src/components/layout/work/ourCommunities.tsx
@@ -1,5 +1,10 @@
+import reddit from '../../../../public/images/work/communities/reddit.png';
+import ara from '../../../../public/images/work/communities/ARA.png';
+
+import CustomImage from 'components/decoration/customImage';
 import SquareField from 'components/decoration/squares';
 import { SectionHeader } from 'components/decoration/textBlocks';
+import { formatNumber } from 'lib/helpers/format';
 
 const TOP_DECORATION_SQUARES = [
   { color: '#454545', size: 16, left: 0, top: 0 },
@@ -19,11 +24,27 @@ const OurCommunities: React.FC = () => {
 
       <div className="relative w-full overflow-hidden text-2xl text-white bg-[#292929]">
         <div className="relative flex flex-col px-2 py-20 mx-auto md:w-1/2 gap-y-8">
-          <SectionHeader className="mb-2" header={['Our', 'COMMUNITIES']} />
-          <p>
+          <SectionHeader className="mb-2" header={['Our', 'COMMUNITIES']}>
             We manage/run these large communities of passionate vegans and
             activists, helping us empower a new age of volunteers and activism
-          </p>
+          </SectionHeader>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 text-left gap-4 w-full">
+            <div>
+              <div>
+                <CustomImage src={reddit} alt="Reddit" />
+              </div>
+              <div className="font-bold">Vegans of Reddit</div>
+              <div>{formatNumber(1200000)}+ members</div>
+            </div>
+            <div>
+              <div>
+                <CustomImage src={ara} alt="Animal Rights Advocates" />
+              </div>
+              <div className="font-bold">Animal Rights Advocates</div>
+              <div>{formatNumber(25000)}+ members</div>
+            </div>
+          </div>
         </div>
       </div>
 
