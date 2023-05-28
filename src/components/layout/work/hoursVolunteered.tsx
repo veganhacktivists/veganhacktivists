bf61f76b4bb73f474703283184f4ef9ee5ffd677
David Price <davidwillprice@gmail.com>
2023-05-28 20:28:04 +0100

    Updated hoursVolunteered Layout
    - Centered text on desktop to match Figma
    - Made into one column at wider resolution
    - Ensured text isn't covered by decorative blocks

--- src/components/layout/work/hoursVolunteered.tsx
+++ src/components/layout/work/hoursVolunteered.tsx
@@ -28,12 +28,12 @@ const HoursVolunteered: React.FC = () => {
 
       <div className="relative w-full overflow-hidden text-2xl bg-white">
         <div>
-          <div className="bg-magenta flex flex-row flex-wrap p-12 px-2">
-            <div className="flex items-center flex-wrap w-full md:w-1/2 text-white text-8xl font-mono px-2 justify-center">
+          <div className="bg-magenta flex flex-col lg:flex-row justify-center md:first-letter:flex-wrap p-12 px-2">
+            <div className="flex items-center flex-wrap  text-white text-8xl font-mono px-2 justify-center">
               <span className="font-bold">9600+</span>{' '}
               <span className="uppercase font-light">Hours</span>
             </div>
-            <div className="flex items-center flex-wrap w-full md:w-1/2 text-white text-3xl font-serif italic font-bold  text-center md:text-left px-2 justify-center">
+            <div className="flex items-center flex-wrap max-w-lg mx-auto lg:mx-0 lg:max-w-[27rem] text-white text-3xl font-serif italic font-bold text-center lg:text-left px-2 justify-center">
               volunteered in 2022 for the animal protection movement
             </div>
           </div>
