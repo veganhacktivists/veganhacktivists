82bf8a4d299d400afc92b8677d13c5391cad842e
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-05-29 11:44:36 +0200

    Add autoscroller

--- src/components/decoration/autoscroll.tsx
+++ src/components/decoration/autoscroll.tsx
@@ -1,121 +1,47 @@
-import { animated, useSprings, config, useTrail } from '@react-spring/web';
-import { useMemo, useRef } from 'react';
+import classNames from 'classnames';
+import { useMemo } from 'react';
 
 export interface AutoScrollProps {
-  items: React.ReactNode[];
-  rows?: number;
+  items: React.ReactNode[] | React.ReactNode[][];
 }
 
 interface AnimatedRowProps {
   items: React.ReactNode[];
   backwards?: boolean;
 }
-const AnimatedRow = ({ items, backwards = false }: AnimatedRowProps) => {
-  const repeats = 2;
-  const x = 100;
-  const [springs, api] = useSprings(items.length * repeats, () => {
-    return {
-      from: {
-        transform: `translateX(${x / repeats}%)`, //  `translateX(0%)` // backwards ? `translateX(${x}%)` : `translateX(${-x}%)`,
-        // left: backwards ? '0%' : '0',
-      },
-      to: {
-        transform: backwards ? `translateX(${-x}%)` : `translateX(${x}%)`,
-        // left: backwards ? '-10%' : '110%',
-      },
-      loop: true,
-      // reverse: backwards,
-      config: {
-        // ...config.molasses,
-        // progress: 0.5,
-        duration: 2000, // * items.length,
-      },
-      // onRest: (a, controller) => {
-      //   console.log('rest');
 
-      //   controller.set({ transform: 'translateX(0%)' });
-      // },
-    };
-  });
-
-  const duplicatedItems = useMemo(() => {
-    return Array(repeats)
-      .fill(true)
-      .flatMap((_, j) =>
-        items.map((item, i) => (
-          <animated.div
-            key={`${i}-${j}`}
-            // className="group-even:-translate-x-1/2 flex-shrink-0 relative"
-            className="flex-shrink-0 relative"
-            style={springs[i]}
-          >
-            {item}
-          </animated.div>
-        ))
-      );
-  }, [items, springs]);
-
+const AnimatedRow = ({ items, backwards }: AnimatedRowProps) => {
   return (
-    <div className="flex flex-row flex-shrink-0 flex-nowrap justify-start gap-3 group relative w-[200%]">
-      {duplicatedItems}
+    <div
+      className={classNames(
+        'flex w-fit justify-center flex-row flex-1',
+        backwards
+          ? 'animate-infinite-scroll-reverse'
+          : 'animate-infinite-scroll'
+      )}
+    >
+      {[...items, ...items].map((item, i) => (
+        <li key={i} className="max-w-xs w-fit flex-shrink-0 basis-full">
+          {item}
+        </li>
+      ))}
     </div>
   );
 };
 
-export const AutoScroll = ({ items, rows: rowNumber = 3 }: AutoScrollProps) => {
-  const itemsPerRow = Math.ceil(items.length / rowNumber);
-
-  const rows = useMemo(() => {
-    return Array(rowNumber)
-      .fill(true)
-      .map((_, i) => {
-        return items.slice(i * itemsPerRow, (i + 1) * itemsPerRow);
-      });
-  }, [items, itemsPerRow, rowNumber]);
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  // const { width: containerWidth } = useWindowSize(containerRef);
-
-  const animatedRows = useMemo(
-    () =>
-      rows
-        .slice(0, 1)
-        .map((row, i) => (
-          <AnimatedRow key={i} items={row} backwards={i % 2 !== 0} />
-        )),
-    [rows]
+export const Autoscroll = ({ items }: AutoScrollProps) => {
+  const rows = useMemo(
+    () => (Array.isArray(items[0]) ? items : [items]) as React.ReactNode[][],
+    [items]
   );
-  // const animatedRows = useMemo(
-  //   () =>
-  //     rows.map((row, rowIndex) => (
-  //       <div
-  //         className="flex flex-row flex-nowrap gap-3 group flex-shrink-0 justify-center relative w-full"
-  //         key={rowIndex}
-  //       >
-  //         {row.flatMap((item, i) =>
-  //           Array(repeats)
-  //             .fill(true)
-  //             .map((_, inner) => (
-  //               <animated.div
-  //                 key={`${i}-${inner}`}
-  //                 style={springs[rowIndex * itemsPerRow + i]}
-  //                 className="group-even:-translate-x-1/2 flex-shrink-0"
-  //               >
-  //                 {item}
-  //               </animated.div>
-  //             ))
-  //         )}
-  //       </div>
-  //     )),
-  //   [itemsPerRow, rows, springs]
-  // );
 
   return (
-    <div
-      className="flex flex-col gap-3 flex-nowrap overflow-x-hidden relative w-[200%]"
-      ref={containerRef}
-    >
-      {animatedRows}
+    <div className="overflow-hidden w-full">
+      <ul className="grayscale relative">
+        {rows.map((row, i) => (
+          <AnimatedRow key={i} items={row} backwards={i % 2 === 0} />
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/layout/work/ourWork.tsx
+++ src/components/layout/work/ourWork.tsx
@@ -1,9 +1,164 @@
-import { AutoScroll } from 'components/decoration/autoscroll';
+import { Autoscroll } from 'components/decoration/autoscroll';
 import { DarkButton } from 'components/decoration/buttons';
 import CustomImage from 'components/decoration/customImage';
 import Sprite, { goat } from 'components/decoration/sprite';
 import SquareField from 'components/decoration/squares';
 import { SectionHeader } from 'components/decoration/textBlocks';
+
+import aaa from '~images/work/orgs/aaa.png';
+import aaasia from '~images/work/orgs/aaasia.png';
+import aac from '~images/work/orgs/aac.png';
+import activeforanimals from '~images/work/orgs/activeforanimals.png';
+import adp from '~images/work/orgs/adp.png';
+import ae from '~images/work/orgs/ae.png';
+import aequality from '~images/work/orgs/aequality.png';
+import afa from '~images/work/orgs/afa.png';
+import ajp from '~images/work/orgs/ajp.png';
+import an from '~images/work/orgs/an.png';
+import animallaw from '~images/work/orgs/animallaw.png';
+import apex from '~images/work/orgs/apex.png';
+import are from '~images/work/orgs/are.png';
+import asap from '~images/work/orgs/asap.png';
+import asm from '~images/work/orgs/asm.png';
+import att from '~images/work/orgs/att.png';
+import awl from '~images/work/orgs/awl.png';
+import ayllu from '~images/work/orgs/ayllu.png';
+import beyond from '~images/work/orgs/beyond.png';
+import bfs from '~images/work/orgs/bfs.png';
+import bg from '~images/work/orgs/bg.png';
+import bhakti from '~images/work/orgs/bhakti.png';
+import bvs from '~images/work/orgs/bvs.png';
+import clariteam from '~images/work/orgs/clariteam.png';
+import db from '~images/work/orgs/db.png';
+import don from '~images/work/orgs/don.png';
+import dxe from '~images/work/orgs/dxe.png';
+import east from '~images/work/orgs/east.png';
+import elwoods from '~images/work/orgs/elwoods.png';
+import faunalytics from '~images/work/orgs/faunalytics.png';
+import fnpd from '~images/work/orgs/fnpd.png';
+import fp from '~images/work/orgs/fp.png';
+import fta from '~images/work/orgs/fta.png';
+import gfi from '~images/work/orgs/gfi.png';
+import gi from '~images/work/orgs/gi.png';
+import greenbaum from '~images/work/orgs/greenbaum.png';
+import hafrica from '~images/work/orgs/hafrica.png';
+import harvest from '~images/work/orgs/harvest.png';
+import ivff from '~images/work/orgs/ivff.png';
+import lead from '~images/work/orgs/lead.png';
+import lv from '~images/work/orgs/lv.png';
+import mevs from '~images/work/orgs/mevs.png';
+import mfa from '~images/work/orgs/mfa.png';
+import nsg from '~images/work/orgs/nsg.png';
+import octopus from '~images/work/orgs/octopus.png';
+import owa from '~images/work/orgs/owa.png';
+import pan from '~images/work/orgs/pan.png';
+import pax from '~images/work/orgs/pax.png';
+import pbd from '~images/work/orgs/pbd.png';
+import peta from '~images/work/orgs/peta.png';
+import phauna from '~images/work/orgs/phauna.png';
+import ragriculture from '~images/work/orgs/ragriculture.png';
+import rap from '~images/work/orgs/rap.png';
+import rp from '~images/work/orgs/rp.png';
+import sa from '~images/work/orgs/sa.png';
+import sehati from '~images/work/orgs/sehati.png';
+import sharpen from '~images/work/orgs/sharpen.png';
+import sm from '~images/work/orgs/sm.png';
+import surge from '~images/work/orgs/surge.png';
+import tbf from '~images/work/orgs/tbf.png';
+import tc from '~images/work/orgs/tc.png';
+import tli from '~images/work/orgs/tli.png';
+import tvns from '~images/work/orgs/tvns.png';
+import vcj from '~images/work/orgs/vcj.png';
+import vegfund from '~images/work/orgs/vegfund.png';
+import vh from '~images/work/orgs/vh.png';
+import vjl from '~images/work/orgs/vjl.png';
+import vo from '~images/work/orgs/vo.png';
+import wai from '~images/work/orgs/wai.png';
+import wam from '~images/work/orgs/wam.png';
+import wfs from '~images/work/orgs/wfs.png';
+
+const images = [
+  aaa,
+  aaasia,
+  aac,
+  activeforanimals,
+  adp,
+  ae,
+  aequality,
+  afa,
+  ajp,
+  an,
+  animallaw,
+  apex,
+  are,
+  asap,
+  asm,
+  att,
+  awl,
+  ayllu,
+  beyond,
+  bfs,
+  bg,
+  bhakti,
+  bvs,
+  clariteam,
+  db,
+  don,
+  dxe,
+  east,
+  elwoods,
+  faunalytics,
+  fnpd,
+  fp,
+  fta,
+  gfi,
+  gi,
+  greenbaum,
+  hafrica,
+  harvest,
+  ivff,
+  lead,
+  lv,
+  mevs,
+  mfa,
+  nsg,
+  octopus,
+  owa,
+  pan,
+  pax,
+  pbd,
+  peta,
+  phauna,
+  ragriculture,
+  rap,
+  rp,
+  sa,
+  sehati,
+  sharpen,
+  sm,
+  surge,
+  tbf,
+  tc,
+  tli,
+  tvns,
+  vcj,
+  vegfund,
+  vh,
+  vjl,
+  vo,
+  wai,
+  wam,
+  wfs,
+];
+
+const numberOfRows = 3;
+
+const imageRows = [...new Array(numberOfRows)].map((_, i) => {
+  const itemsPerRow = Math.ceil(images.length / numberOfRows);
+  return images
+    .slice(i * itemsPerRow, (i + 1) * itemsPerRow)
+    .map((image) => <CustomImage key={image.src} src={image} alt="" />);
+});
 
 const TOP_DECORATION_SQUARES = [
   { color: 'yellow', size: 16, left: 0, top: 0 },
@@ -20,18 +175,6 @@ const BOTTOM_DECORATION_SQUARES = [
   { color: 'gray-background', size: 16, left: 0, bottom: 0 },
 ];
 
-const items = Array(10)
-  .fill(true)
-  .map((_, i) => (
-    <CustomImage
-      key={i}
-      height={200}
-      width={300}
-      src="https://picsum.photos/300/200"
-      alt="random"
-    />
-  ));
-
 const OurWork: React.FC = () => {
   return (
     <>
@@ -40,8 +183,8 @@ const OurWork: React.FC = () => {
         className="hidden md:block"
       />
 
-      <div className="relative w-full overflow-hidden text-2xl bg-white">
-        <div className="relative flex flex-col px-2 py-20 gap-y-8">
+      <div className="w-full text-2xl bg-white">
+        <div className="flex flex-col py-20 gap-y-8">
           <div className="md:w-1/2 mx-auto">
             <SectionHeader className="mb-2" header={['Our', 'WORK']} />
             <p>
@@ -50,7 +193,7 @@ const OurWork: React.FC = () => {
               services.
             </p>
           </div>
-          <AutoScroll items={items} />
+          <Autoscroll items={imageRows} />
           <div className="relative mx-auto md:w-fit">
             <DarkButton href="/services" className="font-mono w-fit">
               Explore our services
