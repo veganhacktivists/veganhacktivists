5f44692f8a4d0520ddceb48f8742684ff4a8057e
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-05-30 14:40:52 +0200

    Update design samples pictures

--- src/components/layout/work/designSamples.tsx
+++ src/components/layout/work/designSamples.tsx
@@ -1,41 +1,5 @@
 import { useCallback, useState } from 'react';
 
-import adp from '../../../../public/images/work/designSamples/designs/VH_Design_ADP.png';
-import gyviGali from '../../../../public/images/work/designSamples/designs/VH_Design_Document_GyviGali.png';
-import pbd from '../../../../public/images/work/designSamples/designs/VH_Design_PBD.png';
-import greenbaum from '../../../../public/images/work/designSamples/designs/VH_Design_Greenbaum.png';
-import tc from '../../../../public/images/work/designSamples/designs/VH_Design_TC.png';
-import kerulos from '../../../../public/images/work/designSamples/designs/VH_Design_Document_Kerulos.png';
-import focus from '../../../../public/images/work/designSamples/designs/VH_Design_FP.png';
-import don from '../../../../public/images/work/designSamples/designs/VH_Design_DonStaniford.png';
-import mevs from '../../../../public/images/work/designSamples/designs/VH_Design_MEVS.png';
-import tnsg from '../../../../public/images/work/designSamples/designs/VH_Design_NSG.png';
-import phauna from '../../../../public/images/work/designSamples/designs/VH_Design_Phauna.png';
-import ivff from '../../../../public/images/work/designSamples/designs/VH_Design_IVFF.png';
-import reimagine from '../../../../public/images/work/designSamples/designs/VH_Design_Document_ReimagineAg.png';
-import sharpen from '../../../../public/images/work/designSamples/designs/VH_Design_Sharpen.png';
-import clariteam from '../../../../public/images/work/designSamples/designs/VH_Design_Clariteam.png';
-import sm from '../../../../public/images/work/designSamples/designs/VH_Design_SM.png';
-import law from '../../../../public/images/work/designSamples/designs/VH_Design_ADP.png';
-import twvns from '../../../../public/images/work/designSamples/designs/VH_Design_Document_TWVNS.png';
-import thumb1 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_1.png';
-import thumb2 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_2.png';
-import thumb3 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_3.png';
-import thumb4 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_4.png';
-import thumb5 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_5.png';
-import thumb6 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_6.png';
-import thumb7 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_7.png';
-import thumb8 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_8.png';
-import thumb9 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_9.png';
-import thumb10 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_10.png';
-import thumb11 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_11.png';
-import thumb12 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_12.png';
-import thumb13 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_13.png';
-import thumb14 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_14.png';
-import thumb15 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_15.png';
-import thumb16 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_16.png';
-import thumb17 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_17.png';
-import thumb18 from '../../../../public/images/work/designSamples/thumbnails/VH_portfolio_18.png';
 import Modal from '../modal';
 
 import { SectionHeader } from 'components/decoration/textBlocks';
@@ -45,6 +9,43 @@ import { Carousel } from 'components/decoration/carousel';
 import CustomImage from 'components/decoration/customImage';
 
 import type { StaticImageData } from 'next/image';
+
+import adp from '~images/work/designSamples/designs/VH_Design_ADP.png';
+import gyviGali from '~images/work/designSamples/designs/VH_Design_Document_GyviGali.jpg';
+import pbd from '~images/work/designSamples/designs/VH_Design_PBD.png';
+import greenbaum from '~images/work/designSamples/designs/VH_Design_Greenbaum.jpg';
+import tc from '~images/work/designSamples/designs/VH_Design_TC.png';
+import kerulos from '~images/work/designSamples/designs/VH_Design_Document_Kerulos.jpg';
+import focus from '~images/work/designSamples/designs/VH_Design_FP.png';
+import don from '~images/work/designSamples/designs/VH_Design_DonStaniford.png';
+import mevs from '~images/work/designSamples/designs/VH_Design_MEVS.png';
+import tnsg from '~images/work/designSamples/designs/VH_Design_NSG.png';
+import phauna from '~images/work/designSamples/designs/VH_Design_Phauna.jpg';
+import ivff from '~images/work/designSamples/designs/VH_Design_IVFF.png';
+import reimagine from '~images/work/designSamples/designs/VH_Design_Document_ReimagineAg.jpg';
+import sharpen from '~images/work/designSamples/designs/VH_Design_Sharpen.png';
+import clariteam from '~images/work/designSamples/designs/VH_Design_Clariteam.png';
+import sm from '~images/work/designSamples/designs/VH_Design_SM.jpg';
+import law from '~images/work/designSamples/designs/VH_Design_ADP.png';
+import twvns from '~images/work/designSamples/designs/VH_Design_Document_TWVNS.jpg';
+import thumb1 from '~images/work/designSamples/thumbnails/VH_portfolio_1.png';
+import thumb2 from '~images/work/designSamples/thumbnails/VH_portfolio_2.png';
+import thumb3 from '~images/work/designSamples/thumbnails/VH_portfolio_3.png';
+import thumb4 from '~images/work/designSamples/thumbnails/VH_portfolio_4.png';
+import thumb5 from '~images/work/designSamples/thumbnails/VH_portfolio_5.png';
+import thumb6 from '~images/work/designSamples/thumbnails/VH_portfolio_6.png';
+import thumb7 from '~images/work/designSamples/thumbnails/VH_portfolio_7.png';
+import thumb8 from '~images/work/designSamples/thumbnails/VH_portfolio_8.png';
+import thumb9 from '~images/work/designSamples/thumbnails/VH_portfolio_9.png';
+import thumb10 from '~images/work/designSamples/thumbnails/VH_portfolio_10.png';
+import thumb11 from '~images/work/designSamples/thumbnails/VH_portfolio_11.png';
+import thumb12 from '~images/work/designSamples/thumbnails/VH_portfolio_12.png';
+import thumb13 from '~images/work/designSamples/thumbnails/VH_portfolio_13.png';
+import thumb14 from '~images/work/designSamples/thumbnails/VH_portfolio_14.png';
+import thumb15 from '~images/work/designSamples/thumbnails/VH_portfolio_15.png';
+import thumb16 from '~images/work/designSamples/thumbnails/VH_portfolio_16.png';
+import thumb17 from '~images/work/designSamples/thumbnails/VH_portfolio_17.png';
+import thumb18 from '~images/work/designSamples/thumbnails/VH_portfolio_18.png';
 
 const images: [image: StaticImageData, thumbnail: StaticImageData][] = [
   [adp, thumb1],
