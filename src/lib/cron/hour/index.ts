b99d60935f40b0eefddaa036a80954e3481368b5
MatsThieme <mats.thieme.mt@gmail.com>
2023-07-02 12:36:29 +0200

    Send email notification for unanswered notifications

--- src/lib/cron/hour/index.ts
+++ src/lib/cron/hour/index.ts
@@ -1,5 +1,7 @@
 import { rejectOldRequestsWithoutApplicantsTask } from './rejectOldRequestsWithoutApplicantsTask';
+import { sendInternalEmailNotificationForRequestsWithoutApplicationsTask } from './sendInternalEmailNotificationForRequestsWithoutApplicationsTask';
 
 export function runHourlyTasks(): void {
   void rejectOldRequestsWithoutApplicantsTask();
+  void sendInternalEmailNotificationForRequestsWithoutApplicationsTask();
 }
