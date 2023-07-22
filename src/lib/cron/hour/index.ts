bb37ffce2e40d35083cbbaf8ff3d63bf2aa4a984
MatsThieme <mats.thieme.mt@gmail.com>
2023-07-22 13:03:57 +0200

    Request feedback about applicants after one week

--- src/lib/cron/hour/index.ts
+++ src/lib/cron/hour/index.ts
@@ -1,7 +1,9 @@
 import { rejectOldRequestsWithoutApplicantsTask } from './rejectOldRequestsWithoutApplicantsTask';
 import { sendInternalEmailNotificationForRequestsWithoutApplicationsTask } from './sendInternalEmailNotificationForRequestsWithoutApplicationsTask';
+import { requestPlaygroundApplicantFeedbackTask } from './requestFeedbackTask';
 
 export function runHourlyTasks(): void {
   void rejectOldRequestsWithoutApplicantsTask();
   void sendInternalEmailNotificationForRequestsWithoutApplicationsTask();
+  void requestPlaygroundApplicantFeedbackTask();
 }
