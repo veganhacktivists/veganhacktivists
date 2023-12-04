95b5cada41eb83ec89de84dbc9cdd24cd27f540e
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-04 21:25:10 +0100

    Migrate existing data, add missing CASE to generated view

--- prisma/migrations/20231204172556_db_generated_values/migration.sql
+++ prisma/migrations/20231204172556_db_generated_values/migration.sql
@@ -8,6 +8,9 @@
 BEGIN;
 CREATE TYPE "UserRole_new" AS ENUM ('Applicant', 'Organization', 'Admin');
 ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" TYPE VARCHAR(12);
+UPDATE "User" u SET "role" = 'Applicant' WHERE "role" = 'User' AND EXISTS (SELECT pa."id" FROM "PlaygroundApplication" pa WHERE u."id" = pa."applicantId");
+UPDATE "User" u SET "role" = 'Organization' WHERE "role" = 'User' AND EXISTS (SELECT pr."id" FROM "PlaygroundRequest" pr WHERE u."id" = pr."requesterId");
 ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
 ALTER TYPE "UserRole" RENAME TO "UserRole_old";
 ALTER TYPE "UserRole_new" RENAME TO "UserRole";
@@ -30,6 +33,7 @@ END
 ) STORED,
 ADD COLUMN     "isOrganization" BOOLEAN GENERATED ALWAYS AS (
   CASE
+  WHEN role = 'Organization' THEN true
   WHEN role = 'Admin' THEN true
   ELSE false
 END
