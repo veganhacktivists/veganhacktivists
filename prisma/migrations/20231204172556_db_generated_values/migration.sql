4e613587cb2854a5c33a91c5bab118c142ffd69d
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-05 09:59:15 +0100

    Move back to prisma extensions

--- prisma/migrations/20231204172556_db_generated_values/migration.sql
+++ prisma/migrations/20231204172556_db_generated_values/migration.sql
@@ -11,6 +11,7 @@ ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
 ALTER TABLE "User" ALTER COLUMN "role" TYPE VARCHAR(12);
 UPDATE "User" u SET "role" = 'Applicant' WHERE "role" = 'User' AND EXISTS (SELECT pa."id" FROM "PlaygroundApplication" pa WHERE u."id" = pa."applicantId");
 UPDATE "User" u SET "role" = 'Organization' WHERE "role" = 'User' AND EXISTS (SELECT pr."id" FROM "PlaygroundRequest" pr WHERE u."id" = pr."requesterId");
+UPDATE "User" u SET "role" = 'Applicant' WHERE "role" = 'User';
 ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
 ALTER TYPE "UserRole" RENAME TO "UserRole_old";
 ALTER TYPE "UserRole_new" RENAME TO "UserRole";
@@ -18,24 +19,4 @@ DROP TYPE "UserRole_old";
 COMMIT;
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isAdmin" BOOLEAN GENERATED ALWAYS AS (
-CASE
-  WHEN role = 'Admin' THEN true
-  ELSE false
-END
-) STORED,
-ADD COLUMN     "isApplicant" BOOLEAN GENERATED ALWAYS AS (
-CASE
-  WHEN role = 'Applicant' THEN true
-  WHEN role = 'Admin' THEN true
-  ELSE false
-END
-) STORED,
-ADD COLUMN     "isOrganization" BOOLEAN GENERATED ALWAYS AS (
-  CASE
-  WHEN role = 'Organization' THEN true
-  WHEN role = 'Admin' THEN true
-  ELSE false
-END
-) STORED,
-ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
