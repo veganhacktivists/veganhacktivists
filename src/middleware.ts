429df62085710b411083f106a70fac57cc2f8c41
Headscracher <t.frohme@all4-one.de>
2023-12-06 11:55:54 +0100

    Add user api route + load middlware user data per api route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,11 @@ import { NextRequest, NextResponse } from 'next/server';
 const middleware = async(req: NextRequest) => {
   const { pathname } = req.nextUrl;
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-  if (pathname.startsWith('/playground/admin') && token?.role !== UserRole.Admin) {
+  if (pathname.startsWith('/api')) {
+    return NextResponse.next();
+  }
+  const user = await fetch(new URL('/api/user', req.url), { headers: { cookie: req.headers.get('cookie') ?? '', 'Content-Type': 'application/json' } }).then(res => res.json()).then(res => res.user);
+  if (pathname.startsWith('/playground/admin') && user?.role !== UserRole.Admin) {
     return NextResponse.redirect(new URL('/auth/signin', req.url));
   }
 };
---
+++ src/pages/api/user.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({req});
+  if (!session?.user?.id){
+    return res.status(401).json({});
+  }
+  const user = await prisma.user.findUnique(
+    { 
+      select: { id: true, name: true, email: true, role: true, applicantInformation: true, requestorInformation: true },
+      where: { id: session.user.id },
+    }
+  );
+  if (!user){
+    return res.status(401).json({});
+  }
+  return res.status(200).json({user});
+
+};
+
+export default handler;
