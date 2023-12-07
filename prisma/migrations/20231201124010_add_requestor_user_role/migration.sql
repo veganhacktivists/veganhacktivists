4d35271335c4786dfb6eae7eda9e6efc569b1c1d
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-07 20:16:05 +0100

    Unify migrations, part 2

--- prisma/migrations/20231201124010_add_requestor_user_role/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "UserRole" ADD VALUE 'Requestor';
--- prisma/migrations/20231204135316_add_requestor_contact_email/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `contactEmail` to the `RequestorInformation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "RequestorInformation" ADD COLUMN     "contactEmail" TEXT NOT NULL;
