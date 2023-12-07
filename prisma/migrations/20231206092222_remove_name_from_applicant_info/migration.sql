836856b0187dbbae7f313213aca9ebdb77a148a0
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-07 20:09:36 +0100

    Unify migrations, part 1

--- prisma/migrations/20231206092222_remove_name_from_applicant_info/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `ApplicantInformation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ApplicantInformation" DROP COLUMN "name";
