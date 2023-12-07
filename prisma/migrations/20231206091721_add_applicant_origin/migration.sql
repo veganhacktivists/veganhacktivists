c7f77451e5f46899ae73afbe1801103c38a6eeff
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-07 20:09:36 +0100

    Unify migrations, part 1

--- prisma/migrations/20231206091721_add_applicant_origin/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `origin` to the `ApplicantInformation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "ApplicantInformation" ADD COLUMN     "origin" TEXT NOT NULL;
