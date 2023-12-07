19ef1d5854effe2b968df9975ada2a2cffef716f
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-08 00:44:26 +0100

    Adapt signup page to new changes

--- src/pages/playground/signup.tsx
+++ src/pages/playground/signup.tsx
@@ -1,29 +1,27 @@
-import { UserRole } from '@prisma/client';
-import { getSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
 import RequestorSignup from 'components/layout/signup/RequestorSignup';
 import ApplicantSignup from 'components/layout/signup/ApplicantSignup';
+import Spinner from 'components/decoration/spinner';
 
-import type { GetServerSideProps } from 'next';
+import type { UserRole } from '@prisma/client';
 
 interface SignupProps {
   role: UserRole;
 }
 
-const Signup: React.FC<SignupProps> = ({ role }) => {
-  if (role === UserRole.Requestor || role === UserRole.Admin) {
+const Signup: React.FC<SignupProps> = () => {
+  const { data: session, status } = useSession({ required: true });
+
+  if (status === 'loading') {
+    return <Spinner />;
+  }
+
+  if (session.user?.isRequestor) {
     return <RequestorSignup />;
   }
   return <ApplicantSignup />;
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const session = await getSession({ req });
-  if (!session?.user) {
-    return { redirect: { destination: '/auth/signin', permanent: false } };
-  }
-  return { props: { role: session.user.role } };
 };
 
 export default Signup;
