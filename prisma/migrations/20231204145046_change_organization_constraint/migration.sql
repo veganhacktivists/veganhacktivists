c8fed033776906ad3b1308ab0b24d8bab1edbf5f
Joaquín Triñanes <joaquin@veganhacktivists.org>
2023-12-07 20:16:05 +0100

    Unify migrations, part 2

--- prisma/migrations/20231204145046_change_organization_constraint/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_organizationId_fkey";
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
