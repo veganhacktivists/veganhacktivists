99f3865422d706abf4dd03e35882b3cd8de3f218
Headscracher <t.frohme@all4-one.de>
2023-12-12 14:39:16 +0100

    Add sorting callback to datagrid component

--- src/components/dataGrid.tsx
+++ src/components/dataGrid.tsx
@@ -1,21 +1,26 @@
 import React, { useCallback, useMemo } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 
-import type { CellValueChangedEvent, ColDef } from 'ag-grid-community';
+import type { CellValueChangedEvent, ColDef, SortChangedEvent } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
+
+export interface SortingOptions {
+  column: string;
+  order: string;
+}
 
 interface DataGridProps<T> {
   data: T[];
   columns: ColDef<T>[];
   onPaginationChange?: (page: number, pageSize: number) => void;
-  onSortChange?: (sort: string, order: string) => void;
+  onSortChange?: (sorting: SortingOptions | null) => void;
   onUpdate?: (column: string, data: T) => void;
   onDataChange?: (data: T[]) => void;
   defaultColDef?: ColDef<T>;
 }
 
-const DataGrid = <T,>({ data, columns, defaultColDef: _defaultColDef, onUpdate }: DataGridProps<T>) => {
+const DataGrid = <T,>({ data, columns, defaultColDef: _defaultColDef, onUpdate, onSortChange }: DataGridProps<T>) => {
   const defaultColDef: ColDef<T> = useMemo(() => {
     if (_defaultColDef) return _defaultColDef;
     return {
@@ -29,12 +34,19 @@ const DataGrid = <T,>({ data, columns, defaultColDef: _defaultColDef, onUpdate }
     onUpdate?.(e.column.getColId(), e.data);
   }, [onUpdate]);
 
+  const handleSortChanged = useCallback((e: SortChangedEvent) => {
+    //@ts-ignore
+    const sorting = e.api.sortController.getSortModel();
+    onSortChange?.(sorting[0] ? { column: sorting[0].colId, order: sorting[0].sort } : null);
+  }, [onSortChange]);
+
   return (
     <AgGridReact
       rowData={data}
       columnDefs={columns}
       defaultColDef={defaultColDef}
       onCellValueChanged={handleCellValueChanged}
+      onSortChanged={handleSortChanged}
       gridOptions={{ domLayout: 'autoHeight' }}
       autoSizeStrategy={{ type: 'fitGridWidth' }}
       className="grid-wrapper ag-theme-quartz text-left"
