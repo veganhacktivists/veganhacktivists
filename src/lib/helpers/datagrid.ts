81cc4f3963c208cc71936f588dca888a192457f2
Headscracher <t.frohme@all4-one.de>
2023-12-13 16:45:40 +0100

    Cleanup code + Fix type errors

--- src/lib/helpers/datagrid.ts
+++ src/lib/helpers/datagrid.ts
@@ -1,28 +1,33 @@
-import { FilterOption } from "lib/services/playground/schemas";
+import type { FilterOption } from 'lib/services/playground/schemas';
 
 interface SortingQuery {
   [key: string]: 'asc' | 'desc' | SortingQuery;
 }
 
-export const buildSortingQuery = (sort: string, order: 'asc' | 'desc'): SortingQuery  => {
+export const buildSortingQuery = (
+  sort: string,
+  order: 'asc' | 'desc'
+): SortingQuery => {
   const columns = sort.split('.');
   if (columns.length === 1) {
     return {
       [sort]: order,
     };
   }
-  return {[columns[0]]: buildSortingQuery(columns.slice(1).join('.'), order)};
+  return { [columns[0]]: buildSortingQuery(columns.slice(1).join('.'), order) };
 };
 
 interface UpdateQuery {
-  [key: string | 'update']: UpdateQuery | any;
+  [key: string]: unknown;
 }
 
-
-export const getKeyEntry = (key: string, value: unknown): UpdateQuery | undefined => {
+export const getKeyEntry = (
+  key: string,
+  value: unknown
+): UpdateQuery | undefined => {
   if (typeof value !== 'object' || value === null) {
     if (key === 'id') return undefined;
-    return {[key]: value};
+    return { [key]: value };
   }
   const fields = Object.entries(value) ?? [];
   if (fields.length === 0) return undefined;
@@ -30,76 +35,81 @@ export const getKeyEntry = (key: string, value: unknown): UpdateQuery | undefine
   for (const field of fields) {
     const [k, v] = field;
     if (k === 'id') continue;
-    entries = {...entries, ...getKeyEntry(k, v)};
+    entries = { ...entries, ...getKeyEntry(k, v) };
   }
-  return {[key]: { update: entries}}
-}
+  return { [key]: { update: entries } };
+};
 
 export const buildUpdateQuery = (data: { [key: string]: unknown }) => {
   const keys = Object.keys(data);
   let query: UpdateQuery = {};
   for (const key of keys) {
-    query = {...query, ...getKeyEntry(key, data[key])};
+    query = { ...query, ...getKeyEntry(key, data[key]) };
   }
   return query;
-}
+};
 
 interface FilterQuery {
-  [key: string]: FilterQuery | any;
+  [key: string]: unknown;
 }
 
 const getFilterCondition = (column: string, filter: FilterOption['filter']) => {
   switch (filter.type) {
     case 'contains':
-      return {[column]: {contains: filter.filter, mode: 'insensitive'}};
+      return { [column]: { contains: filter.filter, mode: 'insensitive' } };
     case 'notContains':
-      return {NOT: {[column]: {contains: filter.filter, mode: 'insensitive'}}};
+      return {
+        NOT: { [column]: { contains: filter.filter, mode: 'insensitive' } },
+      };
     case 'blank':
-      return {[column]: ''};
+      return { [column]: '' };
     case 'notBlank':
-      return {NOT: {[column]: ''}};
+      return { NOT: { [column]: '' } };
     case 'notEqual':
-      return {NOT: {[column]: { equals: filter.filter, mode: 'insensitive' } }};
+      return {
+        NOT: { [column]: { equals: filter.filter, mode: 'insensitive' } },
+      };
     case 'equal':
-      return {[column]: filter.filter, mode: 'insensitive'};
+      return { [column]: filter.filter, mode: 'insensitive' };
     case 'startsWith':
-      return {[column]: {startsWith: filter.filter, mode: 'insensitive'}};
+      return { [column]: { startsWith: filter.filter, mode: 'insensitive' } };
     case 'endsWith':
-      return {[column]: {endsWith: filter.filter, mode: 'insensitive'}};
+      return { [column]: { endsWith: filter.filter, mode: 'insensitive' } };
     default:
       return {};
   }
-}
+};
 
-
-const getFilterEntry = (column: string, filter: FilterOption['filter']): FilterQuery  => {
+const getFilterEntry = (
+  column: string,
+  filter: FilterOption['filter']
+): FilterQuery => {
   const chunks = column.split('.');
   if (chunks.length === 1) {
     return getFilterCondition(column, filter);
   }
-  return {[chunks[0]]: getFilterEntry(chunks.slice(1).join('.'), filter)};
-}
-
-
+  return { [chunks[0]]: getFilterEntry(chunks.slice(1).join('.'), filter) };
+};
 
 export const buildFilterQuery = (filters: FilterOption[]): FilterQuery => {
   let query = {};
   for (const filter of filters) {
-    query = {...query, ...getFilterEntry(filter.column, filter.filter)};
+    query = { ...query, ...getFilterEntry(filter.column, filter.filter) };
   }
   return query;
-
 };
 
 const getSearchEntry = (column: string, search: string): FilterQuery => {
-    const chunks = column.split('.');
-    if (chunks.length === 1) {
-      return { [column]: { contains: search, mode: 'insensitive' } };
-    }
-    return { [chunks[0]]: getSearchEntry(chunks.slice(1).join('.'), search) }; 
-}
+  const chunks = column.split('.');
+  if (chunks.length === 1) {
+    return { [column]: { contains: search, mode: 'insensitive' } };
+  }
+  return { [chunks[0]]: getSearchEntry(chunks.slice(1).join('.'), search) };
+};
 
-export const buildSearchQuery = (search: string, columns: string[]): FilterQuery => {
-  return { OR: columns.map((column) => getSearchEntry(column, search))};
-}
-
+export const buildSearchQuery = (
+  search: string,
+  columns: string[]
+): FilterQuery => {
+  return { OR: columns.map((column) => getSearchEntry(column, search)) };
+};
--- src/pages/playground/admin/applications.tsx
+++ src/pages/playground/admin/applications.tsx
@@ -1,12 +1,14 @@
-import DataGrid, { SortingOptions } from 'components/dataGrid';
-import { trpc } from 'lib/client/trpc';
-
-import type { NextPage } from 'next';
-import type { ColDef } from 'ag-grid-community';
 import { ApplicationStatus } from '@prisma/client';
 import { useCallback, useState } from 'react';
-import { ApplicationEntry } from 'server/routers/playground/admin';
-import { FilterOption } from 'lib/services/playground/schemas';
+
+import DataGrid from 'components/dataGrid';
+import { trpc } from 'lib/client/trpc';
+
+import type { SortingOptions } from 'components/dataGrid';
+import type { ApplicationEntry } from 'server/routers/playground/admin';
+import type { NextPage } from 'next';
+import type { ColDef } from 'ag-grid-community';
+import type { FilterOption } from 'lib/services/playground/schemas';
 
 const Applications: NextPage = () => {
   const [pageSize, setPageSize] = useState(20);
@@ -14,28 +16,70 @@ const Applications: NextPage = () => {
   const [sorting, setSorting] = useState<SortingOptions | null>(null);
   const [filters, setFilters] = useState<FilterOption[]>([]);
   const [search, setSearch] = useState('');
-  const { data } = trpc.playground.admin.allApplications.useQuery({sort: sorting, pageSize, page: currentPage, filters, search }, { keepPreviousData: true });
+  const { data } = trpc.playground.admin.allApplications.useQuery(
+    { sort: sorting, pageSize, page: currentPage, filters, search },
+    { keepPreviousData: true }
+  );
   const { mutate } = trpc.playground.admin.updateApplication.useMutation();
   const columns: ColDef<ApplicationEntry>[] = [
     { field: 'id', hide: true },
-    { field: 'request.title', headerName: 'Request', editable: false, filter: 'agTextColumnFilter'},
-    { field: 'request.category', headerName: 'Category', editable: false, filter: 'agTextColumnFilter'},
-    { field: 'applicant.name', headerName: 'Applicant', filter: 'agTextColumnFilter'},
-    { field: 'moreInfo', headerName: 'Description', filter: 'agTextColumnFilter' },
-    { field: 'estimatedTimeDays', headerName: 'Estimated Time (Days)', cellEditor: 'agNumberCellEditor', cellEditorParams: {  min: 1, valueParser: (val: string) => parseInt(val) } },
-    { field: 'status', headerName: 'Status', cellEditor: 'agSelectCellEditor', cellEditorParams: { values: Object.keys(ApplicationStatus) } },
-    { field: 'createdAt', headerName: 'Applied At', cellEditor: 'agDateCellEditor', cellEditorParams: { dateFormat: 'dd/mm/yyyy' } },
+    {
+      field: 'request.title',
+      headerName: 'Request',
+      editable: false,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'request.category',
+      headerName: 'Category',
+      editable: false,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'applicant.name',
+      headerName: 'Applicant',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'moreInfo',
+      headerName: 'Description',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'estimatedTimeDays',
+      headerName: 'Estimated Time (Days)',
+      cellEditor: 'agNumberCellEditor',
+      cellEditorParams: { min: 1, valueParser: (val: string) => parseInt(val) },
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      cellEditor: 'agSelectCellEditor',
+      cellEditorParams: { values: Object.keys(ApplicationStatus) },
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Applied At',
+      cellEditor: 'agDateCellEditor',
+      cellEditorParams: { dateFormat: 'dd/mm/yyyy' },
+    },
   ];
 
-  const handleValueChange = useCallback((column: string, data: ApplicationEntry) => {
-    const { request, ...rest } = data;
-    mutate(rest);
-  }, [mutate]);
+  const handleValueChange = useCallback(
+    (column: string, data: ApplicationEntry) => {
+      const { request, ...rest } = data;
+      mutate(rest);
+    },
+    [mutate]
+  );
 
-  const handlePaginationChange = useCallback((page: number, pageSize: number) => {
-    setCurrentPage(page);
-    setPageSize(pageSize);
-  }, []);
+  const handlePaginationChange = useCallback(
+    (page: number, pageSize: number) => {
+      setCurrentPage(page);
+      setPageSize(pageSize);
+    },
+    []
+  );
 
   if (!data) {
     return <div>Loading...</div>;
@@ -43,7 +87,15 @@ const Applications: NextPage = () => {
 
   return (
     <div className="w-full h-full">
-      <DataGrid data={data} columns={columns} onUpdate={handleValueChange} onSortChange={setSorting} onPaginationChange={handlePaginationChange} onFilterChange={setFilters} onSearchChange={setSearch} />
+      <DataGrid
+        data={data}
+        columns={columns}
+        onUpdate={handleValueChange}
+        onSortChange={setSorting}
+        onPaginationChange={handlePaginationChange}
+        onFilterChange={setFilters}
+        onSearchChange={setSearch}
+      />
     </div>
   );
 };
