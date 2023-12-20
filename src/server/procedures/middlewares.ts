7610bb4c826eb5b0be0582cabdc47a934e72af50
Joaquín Triñanes <hi@joaquint.io>
2023-12-20 19:11:51 +0100

    Remove unneeded casts

--- src/server/procedures/middlewares.ts
+++ src/server/procedures/middlewares.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { TRPCError, experimental_standaloneMiddleware } from '@trpc/server';
 
+import type { Context } from 'server/context';
 import type { z } from 'zod';
 
-export const ctxInput = <T extends z.ZodTypeAny, C extends object>(
+export const ctxInput = <T extends z.ZodTypeAny, C extends Context>(
   getZodType: (ctx: C) => T,
 ) =>
   experimental_standaloneMiddleware<{
@@ -11,9 +12,7 @@ export const ctxInput = <T extends z.ZodTypeAny, C extends object>(
     rawInput: unknown;
     meta: any;
   }>().create(async ({ next, ctx, rawInput }) => {
-    const result = await getZodType(ctx as unknown as C).safeParseAsync(
-      rawInput,
-    );
+    const result = await getZodType(ctx).safeParseAsync(rawInput);
 
     if (result.success) {
       return next({
