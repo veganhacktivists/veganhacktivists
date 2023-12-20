57fccad8fd1a75080c08292e894309931968f7e3
Joaquín Triñanes <hi@joaquint.io>
2023-12-20 18:49:23 +0100

    Fix invalid nextauth context creation

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,8 +12,7 @@ import { OUR_EMAIL_FROM_FORMATTED } from '../../../lib/mail/router';
 import emailClient from 'lib/mail';
 import prisma from 'lib/db/prisma';
 
-import type { Adapter } from 'next-auth/adapters';
-import type { NextApiHandler } from 'next';
+import type { NextApiHandler, NextApiRequest } from 'next';
 import type { SendVerificationRequestParams } from 'next-auth/providers/email';
 import type { NextAuthOptions } from 'next-auth';
 
@@ -37,64 +36,47 @@ const sendVerificationRequest = async (
   });
 };
 
-export const nextAuthOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt',
-  },
-  providers: [
-    EmailProvider({
-      name: 'magic link',
-      server: process.env.EMAIL_SERVER_URL,
-      from: OUR_EMAIL_FROM_FORMATTED,
-      sendVerificationRequest,
-    }),
-  ],
-  pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    verifyRequest: '/auth/verify-request',
-  },
-  callbacks: {
-    jwt: async ({ token }) => {
-      const user = await prisma.user.findUnique({
-        where: {
-          id: token.sub,
-        },
-      });
+const getPrismaAdapter = (req: NextApiRequest) => {
+  const adapter = PrismaAdapter(prisma);
 
-      if (user) {
-        token.isRequestor =
-          user.role === UserRole.Admin || user.role === UserRole.Requestor;
-        token.isApplicant =
-          user.role === UserRole.Admin || user.role === UserRole.Applicant;
-        token.role = user.role;
-        token.email = user.email;
-        token.name = user.name;
-        token.sub = user.id;
-      }
+  const nextQuery = req.query.nextauth as string | undefined;
 
-      return token;
-    },
-    session: ({ session, token }) => {
-      if (session?.user) {
-        if (token.sub) {
-          session.user.id = token.sub;
-        }
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.role = token.role;
-        session.user.isApplicant = token.isApplicant;
-        session.user.isRequestor = token.isRequestor;
-      }
+  if (!nextQuery) {
+    return adapter;
+  }
 
-      delete session.user?.image;
-      return session;
-    },
-  },
+  if (
+    nextQuery.includes('callback') &&
+    // All this logic can probably be avoided with the profile callback of the other OAuth methods
+    // So we're just going to apply it to the email login for now
+    nextQuery.includes('email')
+  ) {
+    const callbackUrl = req.query.callbackUrl
+      ? new URL(req.query.callbackUrl as string)
+      : undefined;
+
+    if (
+      [UserRole.Applicant, UserRole.Requestor].includes(
+        callbackUrl?.searchParams.get('role') as Exclude<UserRole, 'Admin'>,
+      )
+    ) {
+      const role = callbackUrl?.searchParams.get('role') as Exclude<
+        UserRole,
+        'Admin'
+      >;
+      adapter.createUser = async (data) => {
+        return await prisma.user.create({
+          data: { ...data, role },
+        });
+      };
+    }
+  }
+
+  return adapter;
 };
 
-const getNextAuthOptions = (adapter: Adapter): NextAuthOptions => ({
-  adapter,
+export const getNextAuthOptions = (req: NextApiRequest): NextAuthOptions => ({
+  adapter: getPrismaAdapter(req),
   session: {
     strategy: 'jwt',
   },
@@ -151,36 +133,7 @@ const getNextAuthOptions = (adapter: Adapter): NextAuthOptions => ({
 });
 
 const handle: NextApiHandler = async (req, res) => {
-  const adapter = PrismaAdapter(prisma);
-
-  if (
-    (req.query.nextauth as string).includes('callback') &&
-    // All this logic can probably be avoided with the profile callback of the other OAuth methods
-    // So we're just going to apply it to the email login for now
-    (req.query.nextauth as string).includes('email')
-  ) {
-    const callbackUrl = req.query.callbackUrl
-      ? new URL(req.query.callbackUrl as string)
-      : undefined;
-
-    if (
-      [UserRole.Applicant, UserRole.Requestor].includes(
-        callbackUrl?.searchParams.get('role') as Exclude<UserRole, 'Admin'>,
-      )
-    ) {
-      const role = callbackUrl?.searchParams.get('role') as Exclude<
-        UserRole,
-        'Admin'
-      >;
-      adapter.createUser = async (data) => {
-        return await prisma.user.create({
-          data: { ...data, role },
-        });
-      };
-    }
-  }
-
-  return await NextAuth(req, res, getNextAuthOptions(adapter));
+  return await NextAuth(req, res, getNextAuthOptions(req));
 };
 
 export default handle;
