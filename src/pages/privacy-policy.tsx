6160dcfc7eec749f210253c396e66d7739b5bf66
Mats Thieme <mats.thieme.mt@gmail.com>
2024-06-17 14:21:31 +0200

    Add privacy policy

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,374 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className='m-auto px-2 py-16 w-full md:w-[760px] text-left'>
+      <h1 className='text-4xl mb-4'>
+        <span>Privacy Policy</span>
+      </h1>
+      <p>
+        <span>
+          Effective Date: 06/06/2024
+          <br />
+          Vegan Hacktivists, Inc (&quot;we,&quot; &quot;our,&quot; or
+          &quot;us&quot;) is committed to protecting the privacy of our visitors
+          and supporters. This Privacy Policy explains how we collect, use,
+          disclose, and safeguard your information when you visit our website
+          veganhacktivists.org, or any other media form, media channel, mobile
+          website, or mobile application related or connected thereto
+          (collectively, the &quot;Site&quot;).
+        </span>
+      </p>
+      <p>
+        <span>
+          By accessing or using the Site, you signify that you have read,
+          understood, and agree to our collection, storage, use, and disclosure
+          of your personal information as described in this Privacy Policy.
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>1. Information We Collect</span>
+      </h2>
+      <p>
+        <span>
+          We may collect information about you in a variety of ways. The
+          information we may collect on the Site includes:
+        </span>
+      </p>
+      <h3 className='text-2xl mt-4 mb-2'>
+        <span>Personal Data</span>
+      </h3>
+      <p>
+        <span>
+          Personally identifiable information, such as your name, shipping
+          address, email address, and telephone number, and demographic
+          information, such as your age, gender, hometown, and interests, that
+          you voluntarily give to us when you register with the Site, make a
+          donation, sign up for our newsletter, or participate in activities on
+          the Site.
+        </span>
+      </p>
+      <h3 className='text-2xl mt-4 mb-2'>
+        <span>Derivative Data</span>
+      </h3>
+      <p>
+        <span>
+          Information our servers automatically collect when you access the
+          Site, such as your IP address, your browser type, your operating
+          system, your access times, and the pages you have viewed directly
+          before and after accessing the Site.
+        </span>
+      </p>
+      <h3 className='text-2xl mt-4 mb-2'>
+        <span>Financial Data</span>
+      </h3>
+      <p>
+        <span>
+          Financial information, such as data related to your payment method
+          (e.g., valid credit card number, card brand, expiration date) that we
+          may collect when you donate, order, return, exchange, or request
+          information about our services from the Site. We store only very
+          limited, if any, financial information that we collect.
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>2. Use of Your Information</span>
+      </h2>
+      <p>
+        <span>
+          We use the information we collect about you or that you provide to us,
+          including any personal information, for various purposes, including:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span>To provide, operate, and maintain our Site</span>
+        </li>
+        <li className='list-disc'>
+          <span>To improve, personalize, and expand our Site</span>
+        </li>
+        <li className='list-disc'>
+          <span>To understand and analyze how you use our Site</span>
+        </li>
+        <li className='list-disc'>
+          <span>
+            To develop new products, services, features, and functionality
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span>To process your transactions and manage your donations</span>
+        </li>
+        <li className='list-disc'>
+          <span>To send you emails</span>
+        </li>
+        <li className='list-disc'>
+          <span>To find and prevent fraud</span>
+        </li>
+        <li className='list-disc'>
+          <span>To comply with legal obligations</span>
+        </li>
+      </ul>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>3. Disclosure of Your Information</span>
+      </h2>
+      <p>
+        <span>
+          We may share the information we have collected about you in certain
+          situations. Your information may be disclosed as follows:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span className='font-bold'>By Law or to Protect Rights:</span>
+          <span>
+            &nbsp;If we believe the release of information about you is
+            necessary to respond to legal process, to investigate or remedy
+            potential violations of our policies, or to protect the rights,
+            property, and safety of others.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Third-Party Service Providers:</span>
+          <span>
+            &nbsp;We may share your information with third parties that perform
+            services for us or on our behalf, including payment processing, data
+            analysis, email delivery, hosting services, customer service, and
+            marketing assistance.
+          </span>
+        </li>
+      </ul>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>4. Security of Your Information</span>
+      </h2>
+      <p>
+        <span>
+          We use administrative, technical, and physical security measures to
+          help protect your personal information. While we have taken reasonable
+          steps to secure the personal information you provide to us, please be
+          aware that despite our efforts, no security measures are perfect or
+          impenetrable, and no method of data transmission can be guaranteed
+          against any interception or other type of misuse.
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>5. Your Rights</span>
+      </h2>
+      <p>
+        <span>
+          Depending on your location, you may have certain rights regarding your
+          personal information, such as:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span>
+            The right to access the personal information we hold about you
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span>The right to request that we correct any inaccuracies</span>
+        </li>
+        <li className='list-disc'>
+          <span>
+            The right to request deletion of your personal information
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span>
+            The right to restrict the processing of your personal information
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span>The right to data portability</span>
+        </li>
+        <li className='list-disc'>
+          <span>The right to withdraw consent</span>
+        </li>
+      </ul>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>6. Contact Us</span>
+      </h2>
+      <p>
+        <span>
+          If you have questions or comments about this Privacy Policy, please
+          contact us at:
+        </span>
+      </p>
+      <p>
+        <span>
+          <br />
+          Vegan Hacktivists
+        </span>
+      </p>
+      <p>
+        <span>505 Windy Knoll Dr.</span>
+      </p>
+      <p>
+        <span>
+          #876 Mount Airy, MD 21771
+          <br />
+          hello@veganhacktivists.org
+          <br />
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>
+          7. Your Rights Under the California Consumer Privacy Act (CCPA)
+        </span>
+      </h2>
+      <p>
+        <span>
+          If you are a California resident, you have the following rights:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Know:</span>
+          <span>
+            &nbsp;You can request information about the categories and specific
+            pieces of personal data we have collected about you.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Delete:</span>
+          <span>
+            &nbsp;You can request the deletion of your personal data that we
+            have collected.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Opt-Out:</span>
+          <span>
+            &nbsp;You can request that we do not sell your personal data to
+            third parties.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Non-Discrimination:</span>
+          <span>
+            &nbsp;We will not discriminate against you for exercising any of
+            your CCPA rights.
+          </span>
+        </li>
+      </ul>
+      <p>
+        <span>
+          To exercise any of these rights, please contact us at
+          hello@veganhacktivists.org.
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>
+          8. Your Rights Under the General Data Protection Regulation (GDPR)
+        </span>
+      </h2>
+      <p>
+        <span>
+          If you are a resident of the United Kingdom or the European Union, you
+          have the following rights:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Access:</span>
+          <span>
+            &nbsp;You have the right to request access to the personal data we
+            hold about you.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Rectification:</span>
+          <span>
+            &nbsp;You have the right to request that we correct any inaccuracies
+            in your personal data.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Erasure:</span>
+          <span>
+            &nbsp;You have the right to request that we delete your personal
+            data.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Restrict Processing:</span>
+          <span>
+            &nbsp;You have the right to request that we restrict the processing
+            of your personal data.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Data Portability:</span>
+          <span>
+            &nbsp;You have the right to request a copy of your personal data in
+            a structured, commonly used, and machine-readable format.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Object:</span>
+          <span>
+            &nbsp;You have the right to object to the processing of your
+            personal data under certain conditions.
+          </span>
+        </li>
+      </ul>
+      <p>
+        <span>
+          To exercise any of these rights, please contact our Data Protection
+          Officer at james@veganhacktivists.org.
+        </span>
+      </p>
+      <h2 className='text-3xl mt-8 mb-4'>
+        <span>9</span>
+        <span>
+          . Your Rights Under the Personal Information Protection and Electronic
+          Documents Act (PIPEDA)
+        </span>
+      </h2>
+      <p>
+        <span>
+          If you are a resident of Canada, you have the following rights under
+          PIPEDA:
+        </span>
+      </p>
+      <ul className='py-4 pl-8'>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Access:</span>
+          <span>
+            &nbsp;You have the right to access the personal information we hold
+            about you and to know how it has been used and to whom it has been
+            disclosed.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Correction:</span>
+          <span>
+            &nbsp;You have the right to request correction of any inaccuracies
+            in your personal information.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Withdraw Consent:</span>
+          <span>
+            &nbsp;You can withdraw your consent to the collection, use, and
+            disclosure of your personal information at any time, subject to
+            legal or contractual restrictions and reasonable notice.
+          </span>
+        </li>
+        <li className='list-disc'>
+          <span className='font-bold'>Right to Complain:</span>
+          <span>
+            &nbsp;You have the right to complain to the Privacy Commissioner of
+            Canada about our handling of your personal information.
+          </span>
+        </li>
+      </ul>
+      <p>
+        <span>
+          To exercise any of these rights, please contact our Data Protection
+          Officer at james@veganhacktivists.org.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
