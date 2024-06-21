501bfc41bc2e2b78c83af02aaf688727092ae067
Headscracher <t.frohme@all4-one.de>
2024-06-21 10:58:13 +0200

    Remove env-debug api endpoint

--- src/pages/api/env-debug.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiHandler } from 'next';
-
-const handler: NextApiHandler = (_req, res) => {
-  console.info(process.env);
-  res.status(200).json({ status: 'ok' });
-};
-
-export default handler;
