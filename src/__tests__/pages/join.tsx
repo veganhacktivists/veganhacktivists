ac029979acc3991532ef630e1b82665af805a7e9
MatsThieme <mats.thieme.mt@gmail.com>
2024-12-01 11:26:14 +0100

    next 13 upgrade moved files

--- src/__tests__/pages/join.tsx
+++ src/__tests__/pages/join.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Join from '../../pages/join';
+import Join from '../../pages/[locale]/join';
 
 it('should render correctly', () => {
   const { asFragment } = render(<Join />);
