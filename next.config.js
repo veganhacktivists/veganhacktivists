79ff9825fe4dd9e3eff853a409a68d50fc61b759
MatsThieme <mats.thieme.mt@gmail.com>
2024-12-06 13:08:28 +0100

    fix static file serving

--- next.config.js
+++ next.config.js
@@ -37,16 +37,6 @@ const nextConfig = {
       'mailgun.js',
     ],
   },
-  redirects: async () => [
-    {
-      // this redirect is also configured in the redirects configuration.
-      // this was required because nextjs didn't include the file in the
-      //  build if the redirect is not configured here.
-      source: '/research',
-      destination: '/tech-and-data-in-the-movement.pdf',
-      permanent: false,
-    },
-  ],
 };
 
 module.exports = withPWA(nextConfig);
