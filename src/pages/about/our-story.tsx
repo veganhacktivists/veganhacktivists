3d64bacb94f6faca6921997426d8a2c34490063d
MatsThieme <mats.thieme.mt@gmail.com>
2025-04-30 11:26:21 +0200

    update our story page

--- src/pages/about/our-story.tsx
+++ src/pages/about/our-story.tsx
@@ -10,6 +10,7 @@ import AboutLayout from '../../components/layout/about';
 import { pixelPig } from '../../images/separators';
 
 import CustomImage from 'components/decoration/customImage';
+import { DarkButton } from 'components/decoration/buttons';
 
 import type PageWithLayout from '../../types/persistentLayout';
 
@@ -25,16 +26,76 @@ const OurStory: PageWithLayout = () => {
       />
       <FirstSubSection
         header={intl.formatMessage({
-          id: 'page.about.section.our-story.intro.heading',
+          id: 'page.about.section.our-story.heading',
           defaultMessage: 'Our <b>story</b>',
         })}
       >
-        <FormattedMessage
-          id='page.about.section.our-story.intro.paragraph'
-          defaultMessage='In 2019, we launched our first project with a few volunteers who were passionate about activism. We’ve since grown into a community of highly-skilled and professional software engineers, designers, data scientists, and content creators supporting the movement. We started with the hopes of catalyzing people to action and inspiring them to become advocates for animals — and we are committed to this founding principle. Our digital skills fuel our activism.'
-        />
+        <p className='pb-8'>
+          <FormattedMessage
+            id='page.about.section.our-story.paragraph.1'
+            defaultMessage='In just over five years, Vegan Hacktivists (VH) has grown into a leading capacity-building organization, providing free, high-quality tech and creative services for the animal advocacy movement.'
+          />
+        </p>
+        <p className='font-bold pb-8'>
+          <FormattedMessage
+            id='page.about.section.our-story.paragraph.2'
+            defaultMessage='As the movement’s needs evolve, so does VH’s work – which is why you may know us for many different projects.'
+          />
+        </p>
+        <p className='pb-8'>
+          <FormattedMessage
+            id='page.about.section.our-story.paragraph.3'
+            defaultMessage='We began by building innovative tools to support vegans in their outreach, and have since grown into a team that develops expert services for the movement. While building technology remains a core part of our work, our focus has shifted toward solutions that address the movement’s most pressing needs.'
+          />
+        </p>
+        <div>
+          <h4 className='text-2xl font-bold pb-4 text-left'>
+            <FormattedMessage
+              id='page.about.section.our-story.paragraph.4.heading'
+              defaultMessage='Today, we’re especially proud of the flagship programs we’ve designed and continue to manage, including:'
+            />
+          </h4>
+          <ul className='list-disc pl-8 pb-4 text-left'>
+            <li>
+              <FormattedMessage
+                id='page.about.section.our-story.paragraph.4.list.0'
+                defaultMessage='Violet Studios, our sister organization, which offers authentic design and branding services to the movement at no cost.'
+              />
+            </li>
+            <li>
+              <FormattedMessage
+                id='page.about.section.our-story.paragraph.4.list.1'
+                defaultMessage='Granti, a platform that streamlines grantmaking for both funders and grantees.'
+              />
+            </li>
+            <li>
+              <FormattedMessage
+                id='page.about.section.our-story.paragraph.4.list.2'
+                defaultMessage='Playground, a platform that connects professional animal advocates with skilled volunteers.'
+              />
+            </li>
+          </ul>
+          <h4 className='text-2xl font-bold pb-8 text-left'>
+            <FormattedMessage
+              id='page.about.section.our-story.paragraph.4.list.3'
+              defaultMessage='Our growth has been driven by innovation, ability to adapt, and the dedication of our incredible volunteers.'
+            />
+          </h4>
+        </div>
+        <p className='pb-8'>
+          <FormattedMessage
+            id='page.about.section.our-story.paragraph.5'
+            defaultMessage="Many of our team members began as volunteers and have since become part of our core teams at VH and Violet, contributing their skills in expanded and ongoing roles. We're also proud to work with over 2,600 volunteers across our internal and Playground networks. Since 2019, we’ve supported hundreds of organizations worldwide – improving their operations, elevating their brands, and helping them reach wider audiences through advanced digital tools."
+          />
+        </p>
+        <DarkButton href='/services' className='w-fit mx-auto'>
+          <FormattedMessage
+            id='page.about.section.our-story.cta-services'
+            defaultMessage='Learn about our services'
+          />
+        </DarkButton>
       </FirstSubSection>
-      <div className='pb-10 m-10'>
+      {/* <div className='pb-10 m-10'>
         <div className='pb-5'>
           <CustomImage
             src={pixelPig}
@@ -80,7 +141,7 @@ const OurStory: PageWithLayout = () => {
             }}
           />
         </SubSection>
-      </div>
+      </div> */}
     </>
   );
 };
